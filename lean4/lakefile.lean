7fcc6206fa2fa1947195aacc42d7c0221af1df28
Fabian Gloeckle <fgloeckle@fb.com>
2023-03-16 06:34:21 -0700

    Initialize Lean4 project.

---
+++ lean4/lakefile.lean
@@ -0,0 +1,14 @@
+import Lake
+open Lake DSL
+
+package «lean4» {
+  -- add any package configuration options here
+}
+
+require mathlib from git
+  "https://github.com/leanprover-community/mathlib4.git"
+
+@[default_target]
+lean_lib «Lean4» {
+  -- add any library configuration options here
+}
