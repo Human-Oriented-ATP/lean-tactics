613fc86f2604266b162c9e23ad55ed45660a24ad
Matei Mandache <matei.mandache@gmail.com>
2023-03-28 12:40:12 +0100

    WIP: tidy local declarations

--- lean4/Tidying.lean
+++ lean4/Tidying.lean
@@ -3,22 +3,52 @@ import Lean
 open Lean Expr Meta Elab Tactic
 
 -- Don't worry about this secret, it is used to test the import system
-def secret : Nat := 1
+def secret : Nat := 10
 
 def tidyingReduce : Expr → MetaM Expr := fun e =>
   (withTransparency .instances (reduce (skipTypes := false) e))
 
+def metaTidyGoal : MVarId -> MetaM (MVarId) :=
+  fun originalGoal => do
+    let originalType ← originalGoal.getType
+    let newType ← tidyingReduce originalType
+    let newGoal ← mkFreshExprMVar newType
+    originalGoal.assign newGoal
+    return newGoal.mvarId!
+
+def metaTidyDeclarations : MVarId -> MetaM (MVarId) :=
+  fun goal => do
+    let mut goal' := goal
+    for decl in ← getLCtx do
+      if decl.isImplementationDetail then
+        continue
+      let newType ← tidyingReduce decl.type
+      goal' ← goal'.changeLocalDecl decl.fvarId newType
+      -- goal' ← goal'.assert decl.userName newType decl.toExpr
+      -- (_, goal') ← goal'.intro1P
+    return goal'
+
 elab "tidy_goal" : tactic => 
   withMainContext do
-    liftMetaTactic fun originalGoal => do
-      let originalType ← originalGoal.getType
-      let newType ← tidyingReduce originalType
-      let newGoal ← mkFreshExprMVar newType
-      originalGoal.assign newGoal
-      return [newGoal.mvarId!]
+    liftMetaTactic fun goal => do
+      return [← metaTidyGoal goal]
 
-elab "trace_goal" : tactic =>
+elab "trace_state" : tactic => do
+  let goal ← getMainGoal
+  IO.println "Current goal:"
+  IO.println (← goal.getType)
+  goal.withContext do
+    for decl in ← getLCtx do
+      IO.println s!"Hypothesis {decl.userName} of type {decl.type}"
+
+elab "tidy_everything" : tactic =>
   withMainContext do
-    let goal ← getMainGoal
-    IO.println "Current goal:"
-    IO.println (← goal.getType)
+    let goal ← getMainGoal 
+    goal.withContext do
+      liftMetaTactic fun originalGoal => do
+        return [← metaTidyDeclarations (← metaTidyGoal originalGoal)]
+
+#check LocalDecl.applyFVarSubst
+#check FVarSubst
+#check LocalContext
+#check MVarId.changeLocalDecl
--- lean4/tests/test_tidy.lean
+++ lean4/tests/test_tidy.lean
@@ -1,10 +1,9 @@
 import Tidying
 
--- Check we;ve imported tidying correctly
+-- Check we've imported tidying correctly
 #eval secret
 
-example {a b c : Nat} : (a + b) + c = a + (b + c):= by
-  trace_goal
-  tidy_goal
-  trace_goal
+example {a b c : Nat} (h : a + b = b + a): (a + b) + c = a + (b + c):= by
+  trace_state
+  tidy_everything
   apply Nat.add_assoc
