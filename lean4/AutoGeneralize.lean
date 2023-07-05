4e1064146e8fba635c77383839c04530bfe68324
Anshula Gandhi <anshula@users.noreply.github.com>
2023-07-05 15:40:10 +0100

    instantiated the generalization with addition

--- lean4/AutoGeneralize.lean
+++ lean4/AutoGeneralize.lean
@@ -19,11 +19,18 @@ A generalization of the theorem to any binary operation that is assoc & comm
 ---------------------------------------------------------------------------/
 theorem fPermute :  
 ∀ (f : Nat → Nat → Nat) 
-(f_assoc : ∀ (n m p : Nat), f n (f m p) = f (f n m) p) -- n (m p) = (n m) p
+-- (f_assoc : ∀ (n m p : Nat),  f n (f m p) = f (f n m) p ) -- n (m p) = (n m) p
+(f_assoc : ∀ (n m p : Nat),  f (f n m) p = f n (f m p)) -- n (m p) = (n m) p
 (f_comm : ∀ (n m : Nat), f n m = f m n)
 (n m p : Nat), f n (f m p) = f m (f n p) -- n (m p) = m (n p) 
 := by
   intros f f_assoc f_comm n m p
-  rw [f_assoc]
+  rw [← f_assoc]
   rw [f_comm n m]
   rw [f_assoc]
+
+/---------------------------------------------------------------------------
+Checking that we can instantiate the generalization with addition
+---------------------------------------------------------------------------/
+theorem addPermute : ∀ (n m p : Nat), n + (m + p) = m + (n + p) := by
+  apply fPermute _ Nat.add_assoc Nat.add_comm
