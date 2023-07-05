72ba2d324b260c7dcac73eede031e8fcf5d29d82
Mantas Bakšys <39908973+MantasBaksys@users.noreply.github.com>
2023-07-05 11:09:46 +0100

    Create Implementations.lean

---
+++ lean4/Implementations.lean
@@ -0,0 +1,60 @@
+import Mathlib.Tactic.Cases
+
+macro "targetConjunctionSplit" : tactic => `(tactic| apply And.intro) -- `(tactic| constructor)
+
+example (h : p ∧ q) : q ∧ p := by
+  targetConjunctionSplit -- same as `apply And.intro`
+  . exact h.right
+  . exact h.left
+
+macro "targetImplicationSplit" h:ident : tactic => `(tactic| intro $h:ident)
+-- this macro requires the user to supply the name of the hypothesis
+
+example (p : Prop) : p → p := by
+  targetImplicationSplit h -- same as `intro h`
+  exact h
+
+-- macro "hypothesisConjunctionSplit" h:ident hl:ident hr:ident : tactic => `(tactic| {have $hl := ($h).left; have $hr := ($h).right})
+macro "hypothesisConjunctionSplit" h:ident hl:ident hr:ident : tactic => `(tactic| {have ⟨$hl, $hr⟩ := $h; exact ⟨$hr, $hl⟩})
+-- macro "hypothesisConjunctionSplit" h:ident hl:ident hr:ident : tactic => `(tactic| {cases $h with | intro $hl $hr => })
+
+-- prototype 
+example (h : p ∧ q) : q ∧ p := by
+  have h1 := h.right
+  have h2 := h.left
+  exact ⟨h1, h2⟩
+  -- . exact And.right h
+  -- . exact And.left h
+
+example (h : p ∧ q) : q ∧ p := by
+  cases h with
+  | intro p q =>
+    exact ⟨q, p⟩
+
+example (h : p ∧ q) : q ∧ p := by
+  hypothesisConjunctionSplit h hl hr
+
+-- this seems to be the kind of tactic which needs the next move
+-- the second macro doesn't work unless we supply the tactic a
+-- macro "hypothesisDisjunctionSplit" h:ident : tactic => `(tactic| {apply Or.elim $h:ident})
+macro "hypothesisDisjunctionSplit" h:ident a:ident b:ident : tactic => `(tactic| cases' $h:ident with $a:ident $b:ident)
+
+example (h : p ∨ q) : q ∨ p := by
+  apply Or.elim h
+  . intro hp 
+    exact Or.inr hp
+  . intro hq
+    exact Or.inl hq
+
+example (h : p ∨ q) : q ∨ p := by
+  hypothesisDisjunctionSplit h a b
+  . exact Or.inr a
+  . exact Or.inl b
+
+-- If the current target is `¬P`, then `P` is added to the list of hypotheses and the target is replaced by `False`
+macro "negateTarget" h:ident : tactic => `(tactic| intro $h)
+
+example : ¬ P := by
+  negateTarget P
+  
+
