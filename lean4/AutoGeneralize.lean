dae1acc4ba403be91dc18a38c6dbcc0763a87199
Anshula Gandhi <anshula@users.noreply.github.com>
2023-07-05 15:26:07 +0100

    proved generalized thm

--- lean4/AutoGeneralize.lean
+++ lean4/AutoGeneralize.lean
@@ -1,6 +1,29 @@
+"""
+Proof-dependent generalization
+As described in paper 'Generalization in Type Theory Based Proof Assistants' 
+"""
 
+/---------------------------------------------------------------------------
+A theorem that uses associativity and commutativity of multiplication
+---------------------------------------------------------------------------/
 theorem multPermute : ∀ (n m p : Nat), n * (m * p) = m * (n * p) := by
   intros n m p
   rw [← Nat.mul_assoc]
   rw [@Nat.mul_comm n m]
   rw [Nat.mul_assoc]
+#print multPermute -- the proof term
+
+
+/---------------------------------------------------------------------------
+A generalization of the theorem to any binary operation that is assoc & comm
+---------------------------------------------------------------------------/
+theorem fPermute :  
+∀ (f : Nat → Nat → Nat) 
+(f_assoc : ∀ (n m p : Nat), f n (f m p) = f (f n m) p) -- n (m p) = (n m) p
+(f_comm : ∀ (n m : Nat), f n m = f m n)
+(n m p : Nat), f n (f m p) = f m (f n p) -- n (m p) = m (n p) 
+:= by
+  intros f f_assoc f_comm n m p
+  rw [f_assoc]
+  rw [f_comm n m]
+  rw [f_assoc]
