8e88cb9ed5bafa8be489e5cf6dba1134b2874570
ART0 <18333981+0Art0@users.noreply.github.com>
2023-09-15 11:08:00 +0100

    Bump to latest `mathlib`

--- lakefile.lean
+++ lakefile.lean
@@ -9,9 +9,6 @@ package «leanTactics» {
 
 require mathlib from git
   "https://github.com/leanprover-community/mathlib4.git"
-require proofwidgets from git 
-  "https://github.com/EdAyers/ProofWidgets4" @ "v0.0.15"
-
 
 @[default_target]
 lean_lib «LeanTactics» {
