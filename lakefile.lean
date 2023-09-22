d7be1eedeb345ec1a9119ebaef5373b0b3e2a3d4
ART0 <18333981+0Art0@users.noreply.github.com>
2023-09-22 10:49:21 +0100

    Add `Paperproof` dependency

--- lakefile.lean
+++ lakefile.lean
@@ -8,6 +8,8 @@ package «leanTactics» {
 require mathlib from git
   "https://github.com/leanprover-community/mathlib4.git"
 
+require Paperproof from git "https://github.com/Paper-Proof/paperproof.git"@"main"/"lean"
+
 @[default_target]
 lean_lib «LeanTactics» {
   -- add any library configuration options here
