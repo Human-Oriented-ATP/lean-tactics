0679638a51d269ac467d1d1869389817371ff1b2
ART0 <18333981+0Art0@users.noreply.github.com>
2023-09-25 16:54:55 +0100

    Prototype of library search UI

--- LeanTactics/DynamicButtonList.lean
+++ LeanTactics/DynamicButtonList.lean
@@ -1,4 +1,6 @@
 import LeanTactics.DynamicButton
+import TreeMoves.TreeRewrite
+import TreeMoves.TreeRewriteOrd
 
 open ProofWidgets Lean Meta
 
@@ -150,13 +152,49 @@ def lib_rewrite : InfoviewAction :=
           html?={<p> Rewriting... </p>}
           vanish={true} />
     else OptionT.fail
+
+-- TODO: Move
+
+open Widget
+
+def mkDiv: Array Html → Html := 
+  .element "div" #[]
+
+def mkFragment : Array Html → Html :=
+  .element "Fragment" #[]
+
+def Lean.Widget.CodeWithInfos.addDiffs (diffs : AssocList SubExpr.Pos DiffTag) (code : CodeWithInfos) : CodeWithInfos := 
+  code.map fun info ↦
+    match diffs.find? info.subexprPos with
+      | some diff => { info with diffStatus? := some diff }
+      |    none   =>   info
+
+def Lean.Expr.renderWithDiffs (e : Expr) (diffs : AssocList SubExpr.Pos DiffTag) : MetaM Html := do 
+  let e' := (← Widget.ppExprTagged e).addDiffs diffs
+  return <InteractiveCode fmt={e'} />
+
+def Lean.Name.renderWithDiffs (nm : Name) (diffs : AssocList SubExpr.Pos DiffTag) : MetaM Html := do
+  let env ← getEnv
+  let some ci := env.find? nm | failure
+  ci.type.renderWithDiffs diffs
+
+open Jsx in
+@[motivated_proof_move]
+def libRewrite : InfoviewAction := fun props ↦ do
+  let #[⟨goal, .target pos⟩] := props.selectedLocations | OptionT.fail
+  let libSuggestions ← Tree.librarySearchRewrite pos.toArray.toList (← goal.getType)
+  let resultsDisplay : Html ← mkDiv <$> (libSuggestions.mapM <| fun (suggestions, _score) ↦ do return mkDiv #[
+      ← mkDiv <$> suggestions.mapM fun (name, diffs, text) ↦ do return mkFragment #[
+      <DynamicEditButton label={name.toString} range?={props.range} insertion?={text} size={"small"} color={"info"} variant={"outlined"} />,
+      ← name.renderWithDiffs diffs 
+    ]])
+  pure
+    (<DynamicEditButton label={"Rewrite with library result"} html?={
+      <details «open»={true}>
+      <summary className="mv2 pointer">Library rewrite results</summary>
+      {resultsDisplay}
+      </details>} />)
   
 example : 1 = 1 → 1 = 1 ∧ 1 = 2 := by
 motivated_proof
-sorry
-
-
-
-
-
-
+  sorry
