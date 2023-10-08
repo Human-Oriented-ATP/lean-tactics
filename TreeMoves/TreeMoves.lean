bc269c11e5b302310f395ed6e9eb7f931a796002
JovanGerb <jovan.gerbscheid@gmail.com>
2023-10-08 11:02:26 +0100

    temporary unify_forall_exists

--- TreeMoves/TreeMoves.lean
+++ TreeMoves/TreeMoves.lean
@@ -6,3 +6,10 @@ import TreeMoves.TreeInduction
 import TreeMoves.TreeNormalize
 import TreeMoves.PrintTree
 import TreeMoves.TreeNaming
+
+namespace Tree
+
+lemma forall_exists_unify (p : α → α → Prop) : (∀ a, p a a) → ∀ a, ∃ b, p a b :=
+  fun h a => ⟨a, h a⟩
+
+macro "unify_forall_exists" pos:treePos : tactic => `(tactic| lib_apply [1,1,1] $(Lean.mkIdent ``forall_exists_unify) $pos)
