3c2f68042bb5ee846ad49278245da2e74cab092f
ART0 <18333981+0Art0@users.noreply.github.com>
2023-10-11 22:47:44 +0100

    Debugging `Reducer`s: Error with import

--- LeanTactics/ReducersTest.lean
+++ LeanTactics/ReducersTest.lean
@@ -1,13 +1,14 @@
+import LeanTactics.Reducers
 import ProofWidgets.Component.HtmlDisplay
 
 open Lean ProofWidgets Server Html Jsx
 
-structure HtmlReducerRenderingProps where
-  html : Html
-deriving Server.RpcEncodable
+-- structure HtmlReducerRenderingProps where
+--   html : Html
+-- deriving Server.RpcEncodable
 
-@[widget_module]
-def HtmlReducerRendering : Component HtmlReducerRenderingProps where
-  javascript := include_str "../build/js/reducerRendering.js"
+-- @[widget_module]
+-- def HtmlReducerRendering : Component HtmlReducerRenderingProps where
+--   javascript := include_str "../build/js/reducerRendering.js"
 
 #html .ofComponent HtmlReducerRendering ⟨<p> Hello World </p>⟩ #[]
