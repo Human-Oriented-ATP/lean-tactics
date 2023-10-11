09a3b914d64d2d9b963be9165a452f5dc9e3fd07
ART0 <18333981+0Art0@users.noreply.github.com>
2023-10-11 22:41:11 +0100

    Debugging `Reducer`s: Now seems to be working

--- widget/src/reducerRendering.tsx
+++ widget/src/reducerRendering.tsx
@@ -2,11 +2,16 @@ import * as React from "react"
 import { DocumentPosition } from "@leanprover/infoview"
 import HtmlDisplay, { Html } from "./htmlDisplay"
 
+export const RenderingContext = React.createContext<React.Dispatch<React.SetStateAction<Html>> | undefined>(undefined);
+
 export default function HtmlDisplayPanel({pos, html} : {pos: DocumentPosition, html: Html}): JSX.Element {
   const [output, setOutput] = React.useState(html);
 
-  return <details open>
-    <summary className='mv2 pointer'>HTML Display</summary>
-      <HtmlDisplay pos={pos} html={output} />
-    </details>
+  return (
+    <RenderingContext.Provider value={setOutput}>
+        <details open>
+        <summary className='mv2 pointer'>HTML Display</summary>
+        <HtmlDisplay pos={pos} html={output} />
+        </details>
+    </RenderingContext.Provider> );
 }
