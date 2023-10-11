14eddec4e2b1c46ae3a94ebb2397fbd284d68675
ART0 <18333981+0Art0@users.noreply.github.com>
2023-10-11 22:38:58 +0100

    Debugging `Reducer`s: Works with `<details>` and state

--- widget/src/reducerRendering.tsx
+++ widget/src/reducerRendering.tsx
@@ -1,10 +1,12 @@
+import * as React from "react"
 import { DocumentPosition } from "@leanprover/infoview"
 import HtmlDisplay, { Html } from "./htmlDisplay"
 
-export default function HtmlDisplayPanel({pos, html} : {pos: DocumentPosition, html: Html}):
-    JSX.Element {
+export default function HtmlDisplayPanel({pos, html} : {pos: DocumentPosition, html: Html}): JSX.Element {
+  const [output, setOutput] = React.useState(html);
+
   return <details open>
     <summary className='mv2 pointer'>HTML Display</summary>
-      <HtmlDisplay pos={pos} html={html} />
+      <HtmlDisplay pos={pos} html={output} />
     </details>
 }
