d403dd769fc61c16f78bf414f9eeca47be635e4a
ART0 <18333981+0Art0@users.noreply.github.com>
2023-10-11 22:32:26 +0100

    Debugging `Reducer`s: Barebones component fails?

--- widget/src/reducerRendering.tsx
+++ widget/src/reducerRendering.tsx
@@ -3,9 +3,5 @@ import { DocumentPosition } from "@leanprover/infoview"
 import HtmlDisplay, { Html } from "./htmlDisplay"
 
 export default function HtmlReducerRendering({pos, initHtml}:{pos:DocumentPosition, initHtml:Html}) : JSX.Element {
-    const [html, setHtml] = useState(initHtml)
-
-    return (
-            <HtmlDisplay pos={pos} html={html} />
-    );
+    return (<HtmlDisplay pos={pos} html={initHtml} />);
 }
