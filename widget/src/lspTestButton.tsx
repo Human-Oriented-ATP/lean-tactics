0e081aec567c7bb36a3759e19f073617ea384022
ART0 <18333981+0Art0@users.noreply.github.com>
2023-10-16 16:34:22 +0100

    Attempt at fixing async computation in `lspTestButton`

--- widget/src/lspTestButton.tsx
+++ widget/src/lspTestButton.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { RpcContext, DocumentPosition, mapRpcError, useAsyncPersistent } from "@leanprover/infoview";
+import { RpcContext, DocumentPosition, mapRpcError, useAsyncPersistent, useAsync } from "@leanprover/infoview";
 import {RenderingContext} from './reducerRendering';
 import HtmlDisplay, { Html } from './htmlDisplay';
 
@@ -12,16 +12,15 @@ interface LspButtonProps {
 export default function LspTestButton(props:LspButtonProps) {
     const rs = React.useContext(RpcContext)
     const setHtml = React.useContext(RenderingContext) as React.Dispatch<React.SetStateAction<Html>>
-    const onClickButton = async () => {
-        const newHtml = useAsyncPersistent<Html>(
-            async () => {
-                return await rs.call(props.method, props) 
-            }, [rs, props.pos])
-        if (newHtml.state === 'resolved')
-            setHtml(newHtml.value)
-        else if (newHtml.state === 'rejected')
-            console.log(mapRpcError(newHtml.error).message)
-    }
+    const onClickButton = () => {
+        const st = useAsync<Html>(async () => {
+            return await rs.call(props.method, props)
+        }, [rs, props])
+        if (st.state === 'resolved')
+            setHtml(st.value)
+        else if (st.state === 'rejected')
+            console.log(mapRpcError(st.error).message)
+        }
     return (
         <button onClick={onClickButton}>
             {props.label}
