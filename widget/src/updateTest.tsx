8c35e861d983484bef8b31db6f9b6f0cef3974ce
ART0 <18333981+0Art0@users.noreply.github.com>
2023-10-16 22:00:34 +0100

    Modified code for displaying HTML objects

--- widget/src/updateTest.tsx
+++ widget/src/updateTest.tsx
@@ -3,20 +3,10 @@ import ReactDOMServer from 'react-dom/server'
 import { DocumentPosition } from "@leanprover/infoview"
 import HtmlDisplay, { Html } from "./htmlDisplay"
 
-const TestCtx = React.createContext(0)
-
-function TestComponent(props:{pos:DocumentPosition}) {
-    const ctx = React.useContext(TestCtx);
-    return (<p>Displaying {ctx}.</p>);
-}
-
-const testHtml : Html = 
-    { component : ["", "", {}, []] }
-
-export default function(props:{pos:DocumentPosition}) {
+export default function({pos, html}:{pos:DocumentPosition, html:Html}) {
     return (
-    <TestCtx.Provider value={5}>
-        <p>{ReactDOMServer.renderToStaticMarkup(<HtmlDisplay pos={props.pos} html={testHtml} />)}</p>
-    </TestCtx.Provider>
-    );
+        <p>
+            {ReactDOMServer.renderToStaticMarkup(<HtmlDisplay pos={pos} html={html} />)}
+        </p>
+        );
 }
