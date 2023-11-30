9a2bcfbd0796def7122f15bdb75cc43b017aa2b0
ART0 <18333981+0Art0@users.noreply.github.com>
2023-11-30 18:03:58 +0000

    Docs for the unfold tactic

--- MotivatedMoves/ForMathlib/UnfoldPattern.lean
+++ MotivatedMoves/ForMathlib/UnfoldPattern.lean
@@ -6,9 +6,20 @@ import MotivatedMoves.ForMathlib.Utils
 open Lean Server Core Meta MonadExceptOf Elab Tactic
 open ProofWidgets Json Jsx
 
-section
--- Jovan's `tree_rewrite_def`
+/-!
 
+# Point-and-click unfolding
+
+An implementation of a point-and-click tactic
+to unfold patterns at the specified locations.
+
+-/
+
+section
+
+-- Following `tree_rewrite_def` in `MotivatedMoves/Moves/TreeRewriteDef.lean`
+
+/-- If the expression is a projection under binders, calculate the projection. -/
 partial def reduceProjection (e : Expr) : ExceptT MessageData MetaM Expr :=
   e.withAppRev fun f revArgs => match f with
     | .proj _ i b => do
@@ -16,6 +27,7 @@ partial def reduceProjection (e : Expr) : ExceptT MessageData MetaM Expr :=
       reduceProjection (value.betaRev revArgs)
     | _ => return e
 
+/-- Replace a constant under binders by its definition (also taking care of let reductions). -/
 def replaceByDefAux (e : Expr) : ExceptT MessageData MetaM Expr := do
   if let .letE _ _ v b _ := e then return b.instantiate1 v
   e.withAppRev fun f revArgs => match f with
@@ -31,8 +43,7 @@ def replaceByDefAux (e : Expr) : ExceptT MessageData MetaM Expr := do
       if result == e then throw m! "could not find a definition for {e}"
       else return result
 
---
-
+/-- Replace the pattern by its definition at the specified occurrences in the expression. -/
 def replaceByDef (e : Expr) (pattern : AbstractMVarsResult) (occs : Occurrences) : MetaM Expr := do 
   let (_, _, p) ← openAbstractMVarsResult pattern
   let eAbst ← kabstract e p occs
@@ -47,6 +58,7 @@ def replaceByDef (e : Expr) (pattern : AbstractMVarsResult) (occs : Occurrences)
 end
 
 open Parser Tactic Conv in
+/-- A tactic to perform targeted unfolding of patterns. -/
 elab "unfold'" occs:(occs)? p:term loc:(location)? : tactic => withMainContext do
   let pattern ← expandPattern p
   let location := (expandLocation <$> loc).getD (.targets #[] true)
@@ -65,6 +77,7 @@ elab "unfold'" occs:(occs)? p:term loc:(location)? : tactic => withMainContext d
         replaceMainGoal [newGoal])
       (failed := (throwTacticEx `unfold · m!"Failed to unfold pattern {p}."))
 
+/-- The point-and-click front-end logic for the `unfold'` tactic. -/
 @[server_rpc_method]
 def Unfold.rpc (props : InteractiveTacticProps) : RequestM (RequestTask Html) := do
   let some loc := props.selectedLocations.back? | return .pure <p>Select a sub-expression to unfold.</p>
@@ -95,6 +108,9 @@ elab stx:"unfold?" : tactic => do
   savePanelWidgetInfo stx ``Unfold do
     return json% { replaceRange : $(range) }
 
+
+section Demo
+
 def f := Nat.add
 
 def g (n : Nat) := n + 2
@@ -103,4 +119,7 @@ example (h : f 0 0 = g (1 + 1)) : ∀ n : Nat,  f n 1 = f 1 1 := by
   intro n
   unfold' (occs := 1) g (1 + 1) at h
   unfold' (occs := 1) f ?n ?n
+  unfold? -- select `f 0 0`, for instance
   sorry
+
+end Demo
