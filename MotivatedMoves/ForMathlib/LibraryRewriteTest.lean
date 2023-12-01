8e1d4c5f54ddef0d7213d115736517efd612f9ac
ART0 <18333981+0Art0@users.noreply.github.com>
2023-12-01 12:40:46 +0000

    Update library rewrite demo

--- MotivatedMoves/ForMathlib/LibraryRewriteTest.lean
+++ MotivatedMoves/ForMathlib/LibraryRewriteTest.lean
@@ -3,4 +3,4 @@ import Mathlib.Algebra.Group.Defs
 
 example [Group G] (g h : G) : (g * g⁻¹) * h = h := by
   rw (config := { occs := .pos [1] }) [mul_inv_self g]
-  rw (config := { occs := .pos [1] }) [Group.toCancelMonoid.proof_2 h]
+  rw (config := { occs := .pos [1] }) [one_mul h]
