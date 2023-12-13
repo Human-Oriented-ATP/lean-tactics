ce4b87c4bbdb8f0d313c2e411b621deacbf82f3a
Jovan Gerbscheid <jovan.gerbscheid@gmail.com>
2023-12-13 17:46:38 +0100

    improve `tree_search`

--- MotivatedMoves/Moves/TreeSearch.lean
+++ MotivatedMoves/Moves/TreeSearch.lean
@@ -12,23 +12,28 @@ def bindSolvedAndLeft (p pproof tree : Expr) : TreeProof → TreeProof :=
   fun {newTree, proof} =>
   match newTree with
   | none => { proof := mkApp4 (.const ``closed_solve_and_left []) p tree pproof proof }
-  | some newTree => { 
+  | some newTree => {
       proof := mkApp5 (.const ``solve_and_left []) p tree newTree pproof proof
       newTree }
-      
-def start : HashMap Expr Expr := HashMap.empty.insert (.const ``True []) (.const ``trivial [])
 
-partial def visit (delete? : Bool) (hyps : HashMap Expr Expr := start) (e : Expr) : MetaM TreeProof := do
-  match hyps.find? e with
+def start : Std.AssocList Expr Expr := Std.AssocList.nil.cons (.const ``True []) (.const ``trivial [])
+
+/-- `O(n)`. Returns the first entry in the list whose entry satisfies `p`. -/
+@[specialize] def _root_.Std.AssocList.findPM? [Monad m] (p : α → m Bool) : Std.AssocList α β → m (Option (β))
+  | .nil         => return none
+  | .cons k v es => do bif ← p k then return some v else findPM? p es
+
+partial def visit (delete? : Bool) (hyps : Std.AssocList Expr Expr := start) (e : Expr) : MetaM TreeProof := do
+  match ← hyps.findPM? (Lean.Meta.isDefEq e) with
   | some proof => return {proof}
   | none => match e with
 
-    | and_pattern h tree => match hyps.find? h with
+    | and_pattern h tree => match ← hyps.findPM? (Lean.Meta.isDefEq h) with
       | some proof => bindSolvedAndLeft h proof tree <$> visit delete? hyps tree
-      | none => withLocalDeclD `h h fun fvar => bindAndRightDep delete? h fvar  true tree <$> visit delete? (hyps.insert h fvar) tree
+      | none => withLocalDeclD `h h fun fvar => bindAndRightDep delete? h fvar  true tree <$> visit delete? (hyps.cons h fvar) tree
 
-    | imp_pattern h tree          => withLocalDeclD `h h fun fvar => bindImpRightDep delete? h fvar true tree <$> visit delete? (hyps.insert h fvar) tree
-        
+    | imp_pattern h tree          => withLocalDeclD `h h fun fvar => bindImpRightDep delete? h fvar true tree <$> visit delete? (hyps.cons h fvar) tree
+
     | forall_pattern n u α tree   => withLocalDeclD n  α fun fvar => bindForall   n u α fvar true (.lam n α tree .default) =<< visit delete? hyps (tree.instantiate1 fvar)
     | exists_pattern n u α tree   => withLocalDeclD n  α fun fvar => bindExists   n u α fvar true (.lam n α tree .default) =<< visit delete? hyps (tree.instantiate1 fvar)
     | instance_pattern n u α tree => withLocalDeclD n  α fun fvar => bindInstance n u α fvar true (.lam n α tree .default) =<< visit delete? hyps (tree.instantiate1 fvar)
