fe1f40548a53bd0402c4248d1943cf3ee717da7a
Jovan Gerbscheid <jovan.gerbscheid@gmail.com>
2023-12-13 18:25:51 +0100

    relabel buttons

--- MotivatedMoves/GUI/MotivatedProofList.lean
+++ MotivatedMoves/GUI/MotivatedProofList.lean
@@ -18,7 +18,7 @@ open ProofWidgets Lean Meta Server Widget
 open Jsx OptionT
 
 @[motivated_proof_move]
-def tree_apply : InfoviewAction := 
+def tree_apply : InfoviewAction :=
   fun props => do
   let panelProps := props.toPanelWidgetProps
   if (panelProps.selectedLocations.size == 2) then
@@ -27,15 +27,15 @@ def tree_apply : InfoviewAction :=
     let some pos2 := subexprPos[1]? | failure
     let ⟨_, .target subexprPos1⟩ := pos1 | failure
     let ⟨_, .target subexprPos2⟩ := pos2 | failure
-    let text := subexprPos1.toArray.toList.toString ++ " " ++ 
+    let text := subexprPos1.toArray.toList.toString ++ " " ++
                   subexprPos2.toArray.toList.toString
-    pure <DynamicEditButton 
-            label={"Apply"} 
-            range?={props.range} 
+    pure <DynamicEditButton
+            label={"Apply"}
+            range?={props.range}
             html?={<details «open»={true}>
         <summary className="mv2 pointer">{.text "Apply options"}</summary>
               <DynamicEditButton
-                    label = "Delete the hypothesis"
+                    label = "Discard the hypothesis"
                     range? = {props.range}
                     insertion? = {"tree_apply " ++ text}
                     color = {"secondary"} />
@@ -49,7 +49,7 @@ def tree_apply : InfoviewAction :=
   else failure
 
 @[motivated_proof_move]
-def tree_rewrite : InfoviewAction := 
+def tree_rewrite : InfoviewAction :=
   fun props => do
   let panelProps := props.toPanelWidgetProps
   if (panelProps.selectedLocations.size == 2) then
@@ -58,15 +58,15 @@ def tree_rewrite : InfoviewAction :=
     let some pos2 := subexprPos[1]? | failure
     let ⟨_, .target subexprPos1⟩ := pos1 | failure
     let ⟨_, .target subexprPos2⟩ := pos2 | failure
-    let text := (subexprPos1.toArray.toList.toString ++ " " ++ 
+    let text := (subexprPos1.toArray.toList.toString ++ " " ++
               subexprPos2.toArray.toList.toString)
-    pure <DynamicEditButton 
-            label={"Rewrite"} 
-            range?={props.range} 
+    pure <DynamicEditButton
+            label={"Rewrite"}
+            range?={props.range}
             html?={<details «open»={true}>
         <summary className="mv2 pointer">{.text "Rewrite options"}</summary>
               <DynamicEditButton
-                    label = "Delete the hypothesis"
+                    label = "Discard the hypothesis"
                     range? = {props.range}
                     insertion? = {"tree_rewrite " ++ text}
                     color = {"secondary"} />
@@ -80,24 +80,24 @@ def tree_rewrite : InfoviewAction :=
   else failure
 
 @[motivated_proof_move]
-def tree_rewrite_ord : InfoviewAction := 
-  fun props => do 
+def tree_rewrite_ord : InfoviewAction :=
+  fun props => do
     let panelProps := props.toPanelWidgetProps
-    if (panelProps.selectedLocations.size == 2) then 
+    if (panelProps.selectedLocations.size == 2) then
       let subexprPos := panelProps.selectedLocations
       let some pos1 := subexprPos[0]? | failure
       let some pos2 := subexprPos[1]? | failure
       let ⟨_, .target subexprPos1⟩ := pos1 | failure
       let ⟨_, .target subexprPos2⟩ := pos2 | failure
-      let text := (subexprPos1.toArray.toList.toString ++ " " ++ 
+      let text := (subexprPos1.toArray.toList.toString ++ " " ++
               subexprPos2.toArray.toList.toString)
-      pure <DynamicEditButton 
-            label={"Ordered rewrite"} 
-            range?={props.range} 
+      pure <DynamicEditButton
+            label={"Ordered rewrite"}
+            range?={props.range}
             html?={<details «open»={true}>
         <summary className="mv2 pointer">{.text "Ordered rewrite options"}</summary>
               <DynamicEditButton
-                    label = "Delete the hypothesis"
+                    label = "Discard the hypothesis"
                     range? = {props.range}
                     insertion? = {"tree_rewrite_ord " ++ text}
                     color = {"secondary"} />
@@ -107,22 +107,22 @@ def tree_rewrite_ord : InfoviewAction :=
                     insertion? = {"tree_rewrite_ord' " ++ text}
                     color = {"secondary"} />
                     </details>}
-            vanish={true} />        
+            vanish={true} />
     else failure
 
 @[motivated_proof_move]
-def tree_simp : InfoviewAction := 
+def tree_simp : InfoviewAction :=
   fun props => do
     let panelProps := props.toPanelWidgetProps
     if (panelProps.selectedLocations.size == 1) then
       let some pos := panelProps.selectedLocations[0]? | failure
       let ⟨_, .target subexprPos⟩ := pos | failure
       let text := "tree_simp " ++ subexprPos.toArray.toList.toString
-      pure 
-        <DynamicEditButton 
-          label={"Simplify the expression"} 
-          range?={props.range} 
-          insertion?={text} 
+      pure
+        <DynamicEditButton
+          label={"Simplify"}
+          range?={props.range}
+          insertion?={text}
           html?={<p> Simplifying the target... </p>}
           vanish={true} />
     else failure
@@ -130,27 +130,27 @@ def tree_simp : InfoviewAction :=
 @[motivated_proof_move]
 def tree_search : InfoviewAction := fun props => do
   if (props.selectedLocations.size == 0) then
-      pure <DynamicEditButton 
-              label={"Search for redundancy"} 
-              range?={props.range} 
-              insertion?={"tree_search"} 
+      pure <DynamicEditButton
+              label={"Search for redundancy"}
+              range?={props.range}
+              insertion?={"tree_search"}
               html?={<p> Searching the tree... </p>}
-              vanish={true} />  
+              vanish={true} />
   else failure
 
 @[motivated_proof_move]
-def tree_induction : InfoviewAction := 
-  fun props => do 
+def tree_induction : InfoviewAction :=
+  fun props => do
     let panelProps := props.toPanelWidgetProps
     if (panelProps.selectedLocations.size == 1) then
       let some pos := panelProps.selectedLocations[0]? | failure
       let ⟨_, .target subexprPos⟩ := pos | failure
       let text := "tree_induction " ++ subexprPos.toArray.toList.toString
-      pure 
-        <DynamicEditButton 
-          label={"Perform Induction"} 
-          range?={props.range} 
-          insertion?={text} 
+      pure
+        <DynamicEditButton
+          label={"Perform Induction"}
+          range?={props.range}
+          insertion?={text}
           html?={<p> Performing induction... </p>}
           vanish={true} />
     else failure
@@ -159,16 +159,16 @@ def tree_induction : InfoviewAction :=
 
 open Widget
 
-def mkDiv (elems : Array Html) (cfg : Array (String × Json) := #[]) : Html := 
+def mkDiv (elems : Array Html) (cfg : Array (String × Json) := #[]) : Html :=
   .element "div" cfg elems
 
-def Lean.Widget.CodeWithInfos.addDiffs (diffs : AssocList SubExpr.Pos DiffTag) (code : CodeWithInfos) : CodeWithInfos := 
+def Lean.Widget.CodeWithInfos.addDiffs (diffs : AssocList SubExpr.Pos DiffTag) (code : CodeWithInfos) : CodeWithInfos :=
   code.map fun info ↦
     match diffs.find? info.subexprPos with
       | some diff => { info with diffStatus? := some diff }
       |    none   =>   info
 
-def Lean.Expr.renderWithDiffs (e : Expr) (diffs : AssocList SubExpr.Pos DiffTag) : MetaM Html := do 
+def Lean.Expr.renderWithDiffs (e : Expr) (diffs : AssocList SubExpr.Pos DiffTag) : MetaM Html := do
   let e' := (← Widget.ppExprTagged e).addDiffs diffs
   return <InteractiveCode fmt={e'} />
 
@@ -177,24 +177,24 @@ def Lean.Name.renderWithDiffs (nm : Name) (diffs : AssocList SubExpr.Pos DiffTag
   let some ci := env.find? nm | failure
   ci.type.renderWithDiffs diffs
 
-def renderLibrarySearchResults (range : Lsp.Range) (label : String) 
+def renderLibrarySearchResults (range : Lsp.Range) (label : String)
     (results : Array (Array (Name × (AssocList SubExpr.Pos DiffTag) × String) × Nat)) : MetaM Html := do
   let core ← mkDiv <$> results.mapM (renderBlock ·.fst)
-  pure 
+  pure
     <details «open»={true}>
       <summary className="mv2 pointer">{.text label}</summary>
       {core}
-    </details> 
+    </details>
 where
   renderBlock (results : Array _) : MetaM Html := do
     let block ← results.mapM fun (name, diffs, text) ↦ renderResult name diffs text
     return mkDiv (block.push <hr />)
   renderResult (name : Name) (diffs : AssocList SubExpr.Pos DiffTag) (text : String) : MetaM Html := do
-    return mkDiv 
+    return mkDiv
       #[← name.renderWithDiffs diffs,
-         <DynamicEditButton 
-            label={name.toString} 
-            range?={range} 
+         <DynamicEditButton
+            label={name.toString}
+            range?={range}
             insertion?={text}
             variant={"text"}
             color={"info"}
@@ -209,8 +209,8 @@ def libRewrite : InfoviewAction := fun props ↦ do
     let ⟨goal, .target pos⟩ := subexpr | failure
     let libSuggestions ← Tree.librarySearchRewrite (pos.toArray.toList) (← goal.getType)
     pure
-      <DynamicEditButton 
-          label={"Rewrite with a library result"}
+      <DynamicEditButton
+          label={"Rewrite with a theorem"}
           html?={← renderLibrarySearchResults props.range "Library search results" libSuggestions}
           vanish={true} />
   else failure
@@ -223,8 +223,8 @@ def libRewriteOrd : InfoviewAction := fun props ↦ do
     let ⟨goal, .target pos⟩ := subexpr | failure
     let libSuggestions ← Tree.librarySearchRewriteOrd (pos.toArray.toList) (← goal.getType)
     pure
-      <DynamicEditButton 
-          label={"Ordered rewrite with a library result"}
+      <DynamicEditButton
+          label={"Ordered rewrite with a theorem"}
           html?={← renderLibrarySearchResults props.range "Library search results" libSuggestions}
           vanish={true} />
   else failure
@@ -238,17 +238,17 @@ def libApply : InfoviewAction := fun props ↦ do
   let html_delete := ← renderLibrarySearchResults props.range "Library apply results" libSuggestions_delete
   let html_keep := ← renderLibrarySearchResults props.range "Library apply results" libSuggestions_keep
   pure
-    <DynamicEditButton 
-        label={"Apply a library result"} 
+    <DynamicEditButton
+        label={"Apply a theorem"}
         html?={<details «open»={true}>
         <summary className="mv2 pointer">{.text "Library apply options"}</summary>
               <DynamicEditButton
-                    label = "Delete the closed goal"
+                    label = "Discard closed goal"
                     range? = {props.range}
                     html? = {html_delete}
                     color = {"secondary"} />
                     <DynamicEditButton
-                    label = "Preserve the closed goal as a hypothesis"
+                    label = "Keep closed goal as hypothesis"
                     range? = {props.range}
                     html? = {html_keep}
                     color = {"secondary"} />
@@ -257,22 +257,20 @@ def libApply : InfoviewAction := fun props ↦ do
 --TODO check if selected expression starts with `¬`
 @[motivated_proof_move]
 def push_neg : InfoviewAction := fun props ↦ do
-  if (props.selectedLocations.size == 1) then
-    let some subexprPos := props.selectedLocations[0]? | failure
-    let ⟨goal, .target pos⟩ := subexprPos | failure
-    let (goalOuterPosition, goalPos) := Tree.splitPosition pos.toArray.toList
-    -- not sure the next two lines are doing exactly what I want them to
-    let expr : Expr ← Tree.withTreeSubexpr (← goal.getType) goalOuterPosition goalPos (fun _ x => pure x)
-    let (.app (.const `Not _) _) := expr | failure
-    pure
-      <DynamicEditButton 
-          label={"Push the negation through"}
-          range?={props.range} 
-          insertion?={"tree_push_neg " ++ ((SubExpr.Pos.toArray pos).toList).toString}
-          vanish = {false} />
-  else failure
+  unless (props.selectedLocations.size == 1) do failure
+  let some subexprPos := props.selectedLocations[0]? | failure
+  let ⟨goal, .target pos⟩ := subexprPos | failure
+  let (goalOuterPosition, goalPos) := Tree.splitPosition pos.toArray.toList
+  unless (← Tree.withTreeSubexpr (← goal.getType) goalOuterPosition goalPos (fun _ x => pure x))
+    matches Expr.app (.const ``Tree.Not _) _ do failure
+  pure
+    <DynamicEditButton
+        label={"Push the negation"}
+        range?={props.range}
+        insertion?={"tree_push_neg " ++ ((SubExpr.Pos.toArray pos).toList).toString}
+        vanish = {false} />
 
-structure NamingButtonProps where 
+structure NamingButtonProps where
   selectedPos : String
 deriving RpcEncodable
 
@@ -286,9 +284,9 @@ def name : InfoviewAction := fun props ↦ do
     let some subexprPos := props.selectedLocations[0]? | failure
     let ⟨_, .target pos⟩ := subexprPos | failure
     pure
-      <DynamicEditButton 
-          label={"Name the expression"}
-          range?={props.range} 
+      <DynamicEditButton
+          label={"Give a name"}
+          range?={props.range}
           html? = {<NamingButton selectedPos = {pos.toArray.toList.toString}/>}
           vanish = {true} />
   else failure
@@ -299,9 +297,9 @@ def unify : InfoviewAction := fun props ↦ do
     let some subexprPos := props.selectedLocations[0]? | failure
     let ⟨_, .target pos⟩ := subexprPos | failure
     pure
-      <DynamicEditButton 
-          label={"Unify the relation"}
-          range?={props.range} 
+      <DynamicEditButton
+          label={"Unify"}
+          range?={props.range}
           insertion?={"lib_apply refl " ++ pos.toArray.toList.toString}
           vanish = {true} />
   else failure
@@ -312,9 +310,9 @@ def unfold_definition : InfoviewAction := fun props ↦ do
     let some subexprPos := props.selectedLocations[0]? | failure
     let ⟨_, .target pos⟩ := subexprPos | failure
     pure
-      <DynamicEditButton 
-          label={"Unfold definition"}
-          range?={props.range} 
+      <DynamicEditButton
+          label={"Unfold"}
+          range?={props.range}
           insertion?={"tree_rewrite_def " ++ (pos.toArray.toList).toString}
           vanish = {true} />
   else failure
@@ -325,9 +323,9 @@ def unify_forall_exists : InfoviewAction := fun props ↦ do
     let some subexprPos := props.selectedLocations[0]? | failure
     let ⟨_, .target pos⟩ := subexprPos | failure
     pure
-      <DynamicEditButton 
-          label={"Unify an existential with a preceding forall"}
-          range?={props.range} 
+      <DynamicEditButton
+          label={"Unify existential with preceding forall"}
+          range?={props.range}
           insertion?={"unify_forall_exists " ++ (pos.toArray.toList).toString}
           vanish = {true} />
   else failure
@@ -341,9 +339,9 @@ def contrapose_button : InfoviewAction := fun props ↦ do
     let some subexprPos2 := props.selectedLocations[1]? | failure
     let ⟨_, .target pos2⟩ := subexprPos2 | failure
     pure
-      <DynamicEditButton 
-          label={"Contrapose the implication"}
-          range?={props.range} 
+      <DynamicEditButton
+          label={"Contrapose"}
+          range?={props.range}
           insertion?={s! "tree_contrapose {pos1.toArray.toList} {pos2.toArray.toList}"}
           vanish = {true} />
   else failure
@@ -355,9 +353,9 @@ def swap_hyps : InfoviewAction := fun props ↦ do
     let some subexprPos := props.selectedLocations[0]? | failure
     let ⟨_, .target pos⟩ := subexprPos | failure
     pure
-      <DynamicEditButton 
-          label={"Rotate the hypotheses"}
-          range?={props.range} 
+      <DynamicEditButton
+          label={"Swap the hypotheses"}
+          range?={props.range}
           insertion?={"lib_rewrite Imp.swap " ++ (pos.toArray.toList).toString}
           vanish = {true} />
   else failure
--- Tests/Demo.lean
+++ Tests/Demo.lean
@@ -10,3 +10,33 @@ tree_push_neg [1, 1, 1, 1]
 lib_rewrite [1, 1, 2, 0, 1] not_iff [1, 1, 1, 1, 1]
 unify_forall_exists [1, 1, 1]
 lib_apply refl [1, 1, 1, 1, 2]
+
+
+
+/-- The sum of continuous functions is continuous. -/
+example : ∀ (α : Type u), [PseudoMetricSpace α] →
+  ∀ f g : α → ℝ, Continuous f → Continuous g →
+  Continuous (f + g) := by
+motivated_proof
+lib_rewrite [1, 1, 1, 1, 1, 2, 0, 1] Metric.continuous_iff [1, 1, 1, 1, 1, 1, 2]
+lib_rewrite [1, 1, 1, 1, 1, 2, 0, 1] Metric.continuous_iff [1, 1, 1, 1, 1, 0, 2]
+lib_rewrite [1, 1, 1, 1, 1, 2, 0, 1] Metric.continuous_iff [1, 1, 1, 1, 0, 2]
+lib_rewrite [1, 1, 1, 1, 1, 1, 2, 0, 1] Pi.add_apply [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 0, 1, 0, 1]
+lib_rewrite [1, 1, 1, 1, 1, 1, 2, 0, 1] Pi.add_apply [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 0, 1, 1]
+lib_rewrite [1, 1, 2, 0, 1] Real.dist_eq [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 0, 1]
+lib_rewrite [1, 1, 1, 1, 1, 1, 2, 1] sub_add_sub_comm [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 0, 1, 1]
+lib_rewrite_ord [1, 1, 1, 1] abs_add [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 0, 1]
+lib_rewrite [1, 1, 2, 1] Real.dist_eq [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 0, 1, 0, 1]
+lib_rewrite [1, 1, 2, 1] Real.dist_eq [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 0, 1, 1]
+tree_rewrite_ord [1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 2] [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 0, 1, 0, 1]
+tree_rewrite_ord [1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 2] [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 0, 1, 1]
+lib_apply  [1, 1, 1, 1, 1, 1, 0] exists_add_lt_and_pos_of_lt [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2]
+lib_rewrite [1, 1, 1, 1, 1, 2, 1] lt_min_iff [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+tree_apply [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 2] [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2]
+lib_rewrite [1, 1, 1, 1, 1, 2, 0, 1] lt_min_iff [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2]
+tree_search
+lib_rewrite [1, 1, 2, 0, 1] and_comm [1, 1, 1]
+lib_rewrite [1, 1, 1, 1, 1, 2, 1] Set.mem_Ioo [1, 1, 1]
+lib_rewrite [1, 1, 2, 1] Set.nonempty_def [1, 1]
+lib_rewrite [1, 1, 1, 1, 1, 2, 0, 1] Set.nonempty_Ioo [1, 1, 2]
+tree_apply [1, 0, 2] [1, 1, 2]
