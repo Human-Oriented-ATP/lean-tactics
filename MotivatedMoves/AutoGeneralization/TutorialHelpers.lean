079ddf1c4896f27f38e07842a3f213a4f1f37cd8
Anshula Gandhi <anshula@users.noreply.github.com>
2023-12-22 20:05:24 -0500

    added a file consolidating the most useful helper functions from the tutorial

---
+++ MotivatedMoves/AutoGeneralization/TutorialHelpers.lean
@@ -0,0 +1,81 @@
+import Lean
+import Mathlib.Tactic.Contrapose
+
+open Lean Elab Tactic Meta
+
+/-
+Helper functions that make Lean 4 Metaprogramming a bit more intuitive.
+-/
+
+/-- Return goal variable -/
+def getGoalVar : TacticM MVarId := do
+  return ← getMainGoal
+
+/--  Return goal declaration-/
+def getGoalDecl : TacticM MetavarDecl := do
+  return ← getMainDecl -- (← getGoalVar).getDecl
+
+/-- Return goal expression (the type) -/
+def getGoalType : TacticM Expr := do
+  return ← getMainTarget -- (← getGoalDecl).type or (← getGoalVar).getType
+
+/-- Create a new goal -/
+def createGoal (goalType : Expr) : TacticM Unit := do
+  let goal ← mkFreshExprMVar goalType
+  appendGoals [goal.mvarId!]
+
+/--  Return hypotheses declarations-/
+def getHypotheses : MetaM (List LocalDecl) := do
+  let mut hypotheses : List LocalDecl := []
+  for ldecl in ← getLCtx do
+    if ldecl.isImplementationDetail then continue
+    hypotheses := ldecl :: hypotheses
+  return hypotheses
+
+/-- Create a new hypothesis -/
+def createHypothesis (hypType : Expr) (hypProof : Expr) : TacticM Unit := do
+  let hypName := `h
+  let hyp : Hypothesis := { userName := hypName, type := hypType, value := hypProof }
+  let (_, new_goal) ← (←getGoalVar).assertHypotheses (List.toArray [hyp])
+  setGoals [new_goal]
+
+/--  Tactic to return T/F depending on the type of an expression -/
+def isNat (e: Expr): MetaM Bool := do
+  let type_expr ← inferType e
+  return type_expr.isConstOf `Nat
+
+/-- Given the compiled code in the goal, _print_ the full raw format of an expression  --/
+elab "print_goal_as_expression" : tactic => do
+  let goal ← getGoalType
+  logInfo (repr goal)
+
+/-- What the expression looks like --/
+def logExpression (e : Expr) : MetaM Unit := do
+  dbg_trace "{repr e}"
+
+/-- What the expression looks like, but prettier  --/
+def logPrettyExpression (e : Expr) : MetaM Unit := do
+  dbg_trace "{←ppExpr e}"
+
+/-- Getting theorem statement from context --/
+def getTheoremStatement (n : Name) : MetaM Expr := do
+  let some thm := (← getEnv).find? n | failure -- get the declaration with that name
+  return thm.type -- return the theorem statement
+
+/-- Getting theorem proof from context --/
+def getTheoremProof (n : Name) : MetaM Expr := do
+  let some thm := (← getEnv).find? n | failure -- get the declaration with that name
+  return thm.value! -- return the theorem statement
+
+/- Get (in a list) all subexpressions in an expression -/
+def getSubexpressionsIn (e : Expr) : MetaM (List Expr) :=
+  let rec getSubexpressionsInRec (e : Expr) (acc : List Expr) : MetaM (List Expr) :=
+    match e with
+    | Expr.forallE _ d b _   => return [e] ++ (← getSubexpressionsInRec d acc) ++ (← getSubexpressionsInRec b acc)
+    | Expr.lam _ d b _       => return [e] ++ (← getSubexpressionsInRec d acc) ++ (← getSubexpressionsInRec b acc)
+    | Expr.letE _ t v b _    => return [e] ++ (← getSubexpressionsInRec t acc) ++ (← getSubexpressionsInRec v acc) ++ (← getSubexpressionsInRec b acc)
+    | Expr.app f a           => return [e] ++ (← getSubexpressionsInRec f acc) ++ (← getSubexpressionsInRec a acc)
+    | Expr.mdata _ b         => return [e] ++ (← getSubexpressionsInRec b acc)
+    | Expr.proj _ _ b        => return [e] ++ (← getSubexpressionsInRec b acc)
+    | _                      => return acc
+  getSubexpressionsInRec e []
