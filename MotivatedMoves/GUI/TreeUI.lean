0e81f4c2459ae5be98c718b2f6b92347911aed2c
ART0 <18333981+0Art0@users.noreply.github.com>
2024-01-02 11:21:19 +0000

    Minor adjustments to SVG

--- MotivatedMoves/GUI/TreeUI.lean
+++ MotivatedMoves/GUI/TreeUI.lean
@@ -25,19 +25,19 @@ def Svg.TreeNode.toHtml {f : Svg.Frame} (node : TreeNode f) : Array Html :=
     ("fill", node.color.toStringRGB)
     ] #[]
   let text := Html.element "foreignObject" #[
-    ("x", toJson x),
+    ("x", toJson (x + 1)),
     ("y", toJson y),
     ("width", toJson (charWidth * length)),
     ("height", 30)
-  ] #[<p style={json% {"font-family":"Monospace"}}>{.text node.expr.pretty}</p>]
+  ] #[<p style={json% {"font-family":"JetBrains Mono"}}>{.text node.expr.pretty}</p>]
   #[boundary, text]
 
 def frame : Svg.Frame := {
-  xmin := 0, ymin := 0, xSize := 10, width := 500, height := 1000
+  xmin := 0, ymin := 0, xSize := 500, width := 500, height := 500
 }
 
 def node : Svg.TreeNode frame := {
-  point := .px 300 400, expr := .text "qwertyuiop", color := ⟨1.0, 0.6, 0.2⟩, opacity := 0.6
+  point := .px 30 400, expr := .text "Hello World", color := ⟨1.0, 0.6, 0.2⟩, opacity := 0.6
 }
 
 open Jsx Json
