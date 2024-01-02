b2d3732a52f52993d7550b86eb774713b5316300
ART0 <18333981+0Art0@users.noreply.github.com>
2024-01-02 10:49:21 +0000

    Text box with boundary in SVG

---
+++ MotivatedMoves/GUI/TreeUI.lean
@@ -0,0 +1,50 @@
+import ProofWidgets
+
+open Lean Widget ProofWidgets Server
+
+structure Svg.TreeNode (f : Svg.Frame) where
+  point : Svg.Point f
+  expr : CodeWithInfos
+  color : Svg.Color
+  selectable : Bool := true
+  opacity : Float := 1.0
+  highlight : Bool := false
+
+open scoped Jsx in
+def Svg.TreeNode.toHtml {f : Svg.Frame} (node : TreeNode f) : Array Html :=
+  let length := node.expr.pretty.length
+  let (x, y) := node.point.toAbsolute
+  let charWidth := 8 -- the approximate width of a Unicode character in pixels
+  let boundary := Html.element "rect" #[
+    ("x", toJson x),
+    ("y", toJson y),
+    ("width", toJson (charWidth * length)),
+    ("height", 40),
+    ("rx", 10),
+    ("opacity", toJson node.opacity),
+    ("fill", node.color.toStringRGB)
+    ] #[]
+  let text := Html.element "foreignObject" #[
+    ("x", toJson x),
+    ("y", toJson y),
+    ("width", toJson (charWidth * length)),
+    ("height", 30)
+  ] #[<p style={json% {"font-family":"Monospace"}}>{.text node.expr.pretty}</p>]
+  #[boundary, text]
+
+def frame : Svg.Frame := {
+  xmin := 0, ymin := 0, xSize := 10, width := 500, height := 1000
+}
+
+def node : Svg.TreeNode frame := {
+  point := .px 300 400, expr := .text "qwertyuiop", color := ⟨1.0, 0.6, 0.2⟩, opacity := 0.6
+}
+
+open Jsx Json
+
+#html .element "svg"
+    #[("xmlns", "http://www.w3.org/2000/svg"),
+      ("version", "1.1"),
+      ("width", frame.width),
+      ("height", frame.height)]
+    node.toHtml
