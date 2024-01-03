ad26103ddb9975d4d5b530a49f1b5894c695c02d
ART0 <18333981+0Art0@users.noreply.github.com>
2024-01-03 21:20:47 +0000

    Purge trailing rendering code in `DisplayTree`

--- MotivatedMoves/GUI/DisplayTree.lean
+++ MotivatedMoves/GUI/DisplayTree.lean
@@ -260,39 +260,3 @@ partial def toDisplayTree (pol := true) (root := false) : DelabM DisplayTree := 
       failure 
     else descend e 2 do
       return .node (← ppTreeTagged e)
-
-
-#exit
-
-structure TreeDisplay extends PanelWidgetProps where 
-  tree : DisplayTree
-deriving Server.RpcEncodable
-
-@[widget_module]
-def OrdinaryTreeDisplay : Component TreeDisplay where
-  javascript := include_str ".." / ".." / "build" / "js" / "interactiveTreeDisplay.js"
-
-syntax (name := tree_display) "with_tree_display" tacticSeq : tactic
-
-open Meta Elab Tactic in
-@[tactic tree_display]
-def treeDisplay : Tactic
-  | stx@`(tactic| with_tree_display $tacs) => do
-    let t ← Tree.toDisplayTree <| ← makeTree <| ← getMainTarget
-    savePanelWidgetInfo stx ``OrdinaryTreeDisplay do
-      return json% { tree : $(← rpcEncode t) }
-    evalTacticSeq tacs
-  | _ => throwUnsupportedSyntax
-
-example (p : Prop) (q : Nat → Prop) : ∀ x : Nat, ([LE ℕ] → [r: LE ℕ] →  ∀ a : Nat, ¬ ∃ g n : Int, ∃ m:Nat, Nat → q a) → p → ¬ (p → p) → ∃ m h : Nat, q m := by
-  make_tree
-  sorry
-example (p : Prop) : ∀ x : Nat, ∀ y : Nat, ↑x = y := by
-  make_tree
-  with_tree_display
-  sorry
-
-example (a : Nat) : a + a + a + a + a + a = 6 * a := by
-  make_tree
-  with_tree_display
-  sorry
