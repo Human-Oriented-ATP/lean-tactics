bd3c74a8063b66e53cd44554d7c9e1c64348b48c
ART0 <18333981+0Art0@users.noreply.github.com>
2024-01-11 12:10:16 +0000

    Non-interactive `discrTrees` cache script with `--force` flag

--- MotivatedMoves/LibrarySearch/DiscrTreesData.lean
+++ MotivatedMoves/LibrarySearch/DiscrTreesData.lean
@@ -3,16 +3,16 @@ import Mathlib
 
 open Lean Elab Command Tree
 
-def main : IO Unit := do
+def main (args : List String) : IO Unit := do
   let path ← cachePath
-  let stdin ← IO.getStdin
-  let stdout ← IO.getStdout
+
   if ← path.pathExists then
-    stdout.putStrLn s!"The cache already appears to be stored at {path}.\nWould you like to rewrite it? (y / n)"
-    let input ← stdin.getLine
-    unless input.trim = "y" do
-      stdout.putStrLn "Retaining existing cache and exiting ..."
+    unless args = ["--force"] do
+      IO.println s!"The cache already appears to be stored at {path}."
+      IO.println "Retaining existing cache and exiting ..."
       return ()
+    IO.println "Regenerating cache ..."
+
   IO.FS.createDirAll path.parent.get!
 
   initSearchPath (← Lean.findSysroot) [
@@ -28,7 +28,7 @@ def main : IO Unit := do
     |>.run' {} 
     |>.run' {fileName := "", fileMap := default} { env := env }
   match ← out.toIO' with
-  | .ok _ => stdout.putStrLn "\nWritten `DiscrTrees` to file."
+  | .ok _ => IO.println "\nWritten `DiscrTrees` to file."
   | .error e => IO.throwServerError <| ← e.toMessageData.toString 
 where
   pickleDiscrTrees (path : System.FilePath) : MetaM Unit := do
