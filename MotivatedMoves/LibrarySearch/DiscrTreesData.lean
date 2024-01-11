14c632be2ffb6e67e6246d73e8e5d581f869b0da
ART0 <18333981+0Art0@users.noreply.github.com>
2024-01-11 12:05:01 +0000

    Checking for existing `DiscrTree` cache

--- MotivatedMoves/LibrarySearch/DiscrTreesData.lean
+++ MotivatedMoves/LibrarySearch/DiscrTreesData.lean
@@ -3,12 +3,18 @@ import Mathlib
 
 open Lean Elab Command Tree
 
-def storeDiscrTrees : MetaM Unit := do
-  let path ← cachePath
-  IO.FS.createDirAll path.parent.get!
-  pickle path (← getLibraryLemmas).2 `DiscrTreesData
-
 def main : IO Unit := do
+  let path ← cachePath
+  let stdin ← IO.getStdin
+  let stdout ← IO.getStdout
+  if ← path.pathExists then
+    stdout.putStrLn s!"The cache already appears to be stored at {path}.\nWould you like to rewrite it? (y / n)"
+    let input ← stdin.getLine
+    unless input.trim = "y" do
+      stdout.putStrLn "Retaining existing cache and exiting ..."
+      return ()
+  IO.FS.createDirAll path.parent.get!
+
   initSearchPath (← Lean.findSysroot) [
     "build/lib", 
     "lake-packages/mathlib/build/lib/",  
@@ -17,10 +23,13 @@ def main : IO Unit := do
     "lake-packages/aesop/build/lib/", 
     "lake-packages/proofwidgets/build/lib"
     ]
-  let env ← importModules  #[{ module := `Mathlib }] .empty
-  let out := storeDiscrTrees
+  let env ← importModules #[{ module := `Mathlib }] .empty
+  let out := pickleDiscrTrees path
     |>.run' {} 
     |>.run' {fileName := "", fileMap := default} { env := env }
   match ← out.toIO' with
-  | .ok _ => IO.println "Written `DiscrTrees` to file."
+  | .ok _ => stdout.putStrLn "\nWritten `DiscrTrees` to file."
   | .error e => IO.throwServerError <| ← e.toMessageData.toString 
+where
+  pickleDiscrTrees (path : System.FilePath) : MetaM Unit := do
+    pickle path (← getLibraryLemmas).2 `DiscrTreesData
