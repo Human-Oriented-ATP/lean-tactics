e7ddbf365cc141686d186c8b89708a3a166988b2
Anshula Gandhi <anshula@users.noreply.github.com>
2024-01-31 14:03:39 +0000

    have sum_of_vector_sum_is_sum_of_sum_of_vectors

--- MotivatedMoves/AutoGeneralization/cap-set.lean
+++ MotivatedMoves/AutoGeneralization/cap-set.lean
@@ -1,5 +1,6 @@
 import Mathlib.FieldTheory.Finite.GaloisField
 import Mathlib.Data.Real.Basic
+import Mathlib.Data.Vector.Zip
 
 def zero : ZMod 3 := 0
 def one : ZMod 3 := 1
@@ -26,8 +27,12 @@ def e  {n : ℕ} (i : Fin n) : Vector (ZMod 3) n := toVec (fun idx => if idx == 
 /- The density of any subset A in the finite field -/
 noncomputable def density {n : ℕ} (A : Finset (Vector (ZMod 3) n)) : ℝ  :=  A.card / 3^n
 
+#check Vector.zipWith
+
 /- Define what addition looks like for two vectors-/
-instance : Add (Vector (ZMod 3) n) := ⟨Vector.map₂ Add.add⟩
+instance : Add (Vector (ZMod 3) n) :=
+  ⟨Vector.zipWith (fun x y => x + y)⟩
+  -- ⟨Vector.map₂ Add.add⟩
 #eval e (2 : Fin 5) + e (1 : Fin 5)
 
 /- Define what scaling looks like for a vectors-/
@@ -35,19 +40,37 @@ instance : HMul ℕ (Vector (ZMod 3) n) (Vector (ZMod 3) n) := ⟨fun a v => v.m
 #eval 2 *  e (1 : Fin 5) -- [0, 2, 0, 0, 0]
 #eval 3 *  e (1 : Fin 5) -- zeroes out (because we're going mod 3)[0, 0, 0, 0, 0]
 
+/- Define a function that sums all coordinates of a vector -/
+def sum {n : ℕ} (v : Vector (ZMod 3) n) : ZMod 3 := v.toList.sum
+#eval vec_examp -- [0, 1, 2, 0, 1]
+#eval sum vec_examp -- 1
+
 -- Define the sets A₀, A₁, A₂ based on the sum of components modulo 3
-def A₀ (n : ℕ) : Finset (Vector (ZMod 3) n) := Finset.filter (fun v => (v.toList.sum % 3) = 0) Finset.univ
-def A₁ (n : ℕ) : Finset (Vector (ZMod 3) n) := Finset.filter (fun v => (v.toList.sum % 3) = 1) Finset.univ
-def A₂ (n : ℕ) : Finset (Vector (ZMod 3) n) := Finset.filter (fun v => (v.toList.sum % 3) = 2) Finset.univ
+def A₀ (n : ℕ) : Finset (Vector (ZMod 3) n) := Finset.filter (fun v => sum v = 0) Finset.univ
+def A₁ (n : ℕ) : Finset (Vector (ZMod 3) n) := Finset.filter (fun v => sum v = 1) Finset.univ
+def A₂ (n : ℕ) : Finset (Vector (ZMod 3) n) := Finset.filter (fun v => sum v = 2) Finset.univ
 #eval (toVec ![zero, zero, zero, zero, zero]) ∈ A₀ 5 -- true
 #eval (toVec ![zero, zero, zero, zero, one]) ∈ A₀ 5 -- false
 
+/- Lemma: if you have a vector x and a vector y, then sum(x+y) = sum(x) + sum(y) -/
+lemma sum_of_vector_sum_is_sum_of_sum_of_vectors {n : ℕ} {x : Vector (ZMod 3) n} {y : Vector (ZMod 3) n} :
+ sum x + sum y = sum (x+y):=
+by
+  apply Vector.sum_add_sum_eq_sum_zipWith x y
+
 /- Lemma: if you have a vector x ∈ A₀, then x + e i ∉ A₀ -/
 lemma adding_basis_vector_changes_slice {n : ℕ} {x : Vector (ZMod 3) n} :
-  x ∈ A₀ n →  (∀ i : Fin n, x + e i ∉ A₀ n) :=
+  x ∈ A₀ n →  (∀ i : Fin n, x + e i ∈ A₁ n) :=
 by
+  intro h
+  intro i
+  rw A₀ at h
+  simp at h
+  rw A₁
+  exact h
 
-  sorry
+
+
 
 /- A conjecture that you can create a particular line by varying only one coordinate -/
 theorem cap_set_vary_1_coord_not_true :
@@ -64,5 +87,14 @@ by
   -- Prove the set A₀ works
   use A₀
   intros hA x i hx
+
+
+  -- Show that for any x and i in A, either (x + i) or (x + 2 * i) is also in A
+  simp [density, nat.pow_pos (nat.succ_pos 2) n, hδ] at hA,
+  by_cases hi : i.sum % 3 = 0,
+  { left, exact finset.mem_filter.2 ⟨mem_univ _, hi⟩ },
+  by_cases hi1 : i.sum % 3 = 1,
+  { right, exact finset.mem_filter.2 ⟨mem_univ _, hi1⟩ },
+  { left, exact finset.mem_filter.2 ⟨mem_univ _, by linarith⟩ },
 
   sorry
