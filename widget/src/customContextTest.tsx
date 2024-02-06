7be164658004f3d21bf104fb99c2270bde15664f
ART0 <18333981+0Art0@users.noreply.github.com>
2024-02-06 13:23:21 +0000

    Custom context experiments

---
+++ widget/src/customContextTest.tsx
@@ -0,0 +1,20 @@
+import * as React from 'react';
+
+export const CustomContext = React.createContext(0)
+
+export function ComponentA() {
+    let ctx = React.useContext(CustomContext);
+    return <p>Component A with context value {ctx}.</p>
+}
+
+export function ComponentB() {
+    let ctx = React.useContext(CustomContext);
+    return <p>Component B with context value {ctx}.</p>
+}
+
+export function CombinedComponentTest() {
+    <CustomContext.Provider value={5}>
+        <ComponentA />
+        <ComponentB />
+    </CustomContext.Provider>
+}
