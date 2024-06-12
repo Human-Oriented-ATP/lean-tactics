9eef4b004e83b8dddc4e7100578975924ec92a9a
Anshula Gandhi <anshula@users.noreply.github.com>
2024-06-12 10:38:33 +0100

    more code cleanup while debugging

--- MotivatedMoves/AutoGeneralization/AutoGeneralizeDemo3000.lean
+++ MotivatedMoves/AutoGeneralization/AutoGeneralizeDemo3000.lean
@@ -35,61 +35,6 @@ example :  1 + (2 + 3) = 2 + (1 + 3) := by
   specialize _multPermute.Gen 1 2 3
   assumption
 
-/---------------------------------------------------------------------------
-Analogizing the theorem that sqrt(2) is irrational (to the theorem that sqrt(3) is irrational)
----------------------------------------------------------------------------/
-example : Irrational (Real.sqrt 3) := by
-  let _sqrt2Irrational : Irrational (Real.sqrt (2: ℕ)) := by apply Nat.prime_two.irrational_sqrt
-  autogeneralize _sqrt2Irrational (2 : ℕ) -- adds _sqrt2Irrational.Gen to list of hypotheses
-
-  specialize _sqrt2Irrational.Gen 3 (Nat.prime_three)
-  assumption
-
-
-
-/---------------------------------------------------------------------------
-DEMO OF SUCCESS -- The formula for the distance between any two points in ℝ² -- autogeneralize works fine when there's only one instance of what to generalize
----------------------------------------------------------------------------/
-
-example :  ∀ (x y : EuclideanSpace ℝ (Fin 4)), dist x y = sqrt (Finset.sum Finset.univ fun i => dist (x i) (y i) ^ 2) := by
-  let _distance : ∀ (x y : EuclideanSpace ℝ (Fin 3)), dist x y = sqrt (Finset.sum Finset.univ fun i => dist (x i) (y i) ^ 2) := fun x y => EuclideanSpace.dist_eq x y
-  autogeneralize _distance (3:)
-
-  intros x y
-  specialize _distance.Gen 4 x y
-  assumption
-
-/---------------------------------------------------------------------------
-DEMO OF FAILURE -- The formula for the distance between any two points in ℝ² -- now there's two instances of what to generalize...and it overgeneralizes.
----------------------------------------------------------------------------/
-
-example :  ∀ (x y : EuclideanSpace ℝ (Fin 3)), dist x y = sqrt (Finset.sum Finset.univ fun i => dist (x i) (y i) ^ 2) := by
-  let _distance : ∀ (x y : EuclideanSpace ℝ (Fin 2)), dist x y = sqrt (Finset.sum Finset.univ fun i => dist (x i) (y i) ^ 2) := fun x y => EuclideanSpace.dist_eq x y
-
-  autogeneralize _distance (2:)  -- says this formula works for any f-dimensional space as long as distance is given by (∑ i, dist (x i) (y i) ^ f)
-
-  intros x y
-  specialize _distance.Gen 3 x -- x is not a member of a 3-dimensional space such that the distance is given by (∑ i, dist (x i) (y i) ^3)
-  sorry
-
-/---------------------------------------------------------------------------
-DEMO OF SUCCESS-- sqrt(2)+3 is irrational.
----------------------------------------------------------------------------/
-
-example : Irrational (sqrt 2 + 3) := by
-  let _sum_irrat : Irrational (Real.sqrt (2:ℕ) + (4:ℕ)) := by {apply Irrational.add_nat; apply Nat.prime_two.irrational_sqrt}
-  autogeneralize _sum_irrat (4:ℕ)
-
-  specialize _sum_irrat.Gen 3
-  assumption
-
-example : Irrational (sqrt 3 + 4) := by
-  let _sum_irrat : Irrational (Real.sqrt (2:ℕ) + (4:ℕ)) := by {apply Irrational.add_nat; apply Nat.prime_two.irrational_sqrt}
-  autogeneralize _sum_irrat (2:ℕ)
-
-  specialize _sum_irrat.Gen 3 (Nat.prime_three)
-  assumption
-
 /- --------------------------------------------------------------------------
 DEMO OF SUBOPTIMALITY -- sqrt(2)+2 is irrational, generalizes to something over-specific -- prime f -> sqrt(f)+f is irrational
 -------------------------------------------------------------------------- -/
@@ -107,6 +52,7 @@ example : Irrational (Real.sqrt 3) := by
 example : Irrational (Real.sqrt 3 + 2) := by
   let _sum_irrat : Irrational (Real.sqrt (2:ℕ) + (2:ℕ)) := by {apply Irrational.add_nat; apply Nat.prime_two.irrational_sqrt}
   autogeneralize _sum_irrat (2:ℕ)
+  -- autogeneralize _sum_irrat.Gen (2:ℕ)
 
   specialize _sum_irrat.Gen 3 (Nat.prime_three)
   assumption
--- MotivatedMoves/AutoGeneralization/AutoGeneralizeTactic3000.lean
+++ MotivatedMoves/AutoGeneralization/AutoGeneralizeTactic3000.lean
@@ -11,19 +11,6 @@ def getTheoremStatement (n : Name) : MetaM Expr := do
   let some thm := (← getEnv).find? n | throwError ("Could not find a theorem with name " ++ n) -- get the declaration with that name
   return thm.type -- return the theorem statement (the type is the proposition)
 
-/--  Tactic to return hypotheses declarations (including dynamically generated ones)-/
-def getHypotheses : TacticM (List LocalDecl) := do
-  let mut hypotheses : List LocalDecl := []
-  let goal ← getMainGoal  -- the dynamically generated hypotheses are associated with this particular goal
-  for ldecl in (← goal.getDecl).lctx do
-    if ldecl.isImplementationDetail then continue
-    hypotheses := ldecl :: hypotheses
-  return hypotheses
-
-/--  Tactic to return hypotheses names-/
-def getHypothesesNames : TacticM (List Name) := do
-    return (← getHypotheses).map (fun hypothesis => hypothesis.userName)
-
 /--  Tactic get a hypothesis by its name -/
 def getHypothesisByName (h : Name) : TacticM LocalDecl := do
   let goal ← getMainGoal  -- the dynamically generated hypotheses are associated with this particular goal
@@ -56,94 +43,25 @@ def getHypothesisProof (h : Name) : TacticM Expr := do
 def getGoalVar : TacticM MVarId := do
   return ← getMainGoal
 
-/--  Tactic to return goal declaration-/
-def getGoalDecl : TacticM MetavarDecl := do
-  return ← getMainDecl -- (← getGoalVar).getDecl
-
-/--  Tactic to return goal expression (the type) -/
-def getGoalType : TacticM Expr := do
-  return ← getMainTarget -- (← getGoalDecl).type or (← getGoalVar).getType
-
-/-- Create new goals -/
-def createGoal (goalType : Expr) : TacticM Unit := do
-  let goal ← mkFreshExprMVar goalType
-  appendGoals [goal.mvarId!]
-
 /-- Create a new hypothesis using a "have" statement -/
 def createHypothesis (hypType : Expr) (hypProof : Expr) (hypName? : Option Name := none) : TacticM Unit := do
   let hypName := hypName?.getD `h -- use the name given first, otherwise call it `h
   let hyp : Hypothesis := { userName := hypName, type := hypType, value := hypProof }
+  let check ← isDefEq (hypType) (← inferType hypProof)
+  if !check then throwError "Hypothesis type {hypType} doesn't match proof {hypProof}"
   let (_, new_goal) ← (←getGoalVar).assertHypotheses (List.toArray [hyp])
   setGoals [new_goal]
 
 /-- Create a new hypothesis using a "let" statement (so its proof is accessible)-/
 def createLetHypothesis (hypType : Expr) (hypProof : Expr) (hypName? : Option Name := none) : TacticM Unit := do
-  -- (←getGoalVar).withContext do
   let hypName := hypName?.getD `h -- use the name given first, otherwise call it `h
   let new_goal ← (←getGoalVar).define hypName hypType hypProof
   let (_, new_goal) ← intro1Core new_goal true
   setGoals [new_goal]
 
-
-
 /-- Print out an expression in a human-readable way  --/
 def printPrettyExpression (e : Expr) : MetaM Unit := do
   dbg_trace "{←ppExpr e}"
-
-/-- Turn syntax into fully elaborated expressions --/
-def syntaxToExpr (e : TermElabM Syntax) : TermElabM Expr := do
-  let e ← elabTermAndSynthesize (← e) none
-  return e
-#eval syntaxToExpr `(2+3=5)
-
-/-- Turn code into fully elaborated expressions --/
-elab "#term_to_expr" t:term : command => do
-  let e ← liftTermElabM (Term.elabTerm t none)
-  logInfo m!"The expression corresponding to {t} is:\n\n{repr e}"
-#term_to_expr (2+3=5)
-#term_to_expr (Eq.refl 0)
-
-
-
-/-- Given the compiled code in the goal, _print_ the full raw format of an expression  --/
-elab "print_goal_as_expression" : tactic => do
-  let goal ← getGoalType
-  logInfo (toExpr goal) -- or logInfo (repr goalt)
-
-example :1+1=2 := by
-  print_goal_as_expression
-  ring
-
-theorem reflOfZero : 0=0:= by
-  print_goal_as_expression
-  simp
-
-theorem multPermute : ∀ (n m p : Nat), n * (m * p) = m * (n * p) := by
-  print_goal_as_expression
-  ring; simp
-
-/-- What the expression looks like --/
-def logExpression (e : Expr) : MetaM Unit := do
-  dbg_trace "{repr e}"
-
-/-- What the expression looks like, but prettier  --/
-def logFormattedExpression (e : Expr) : MetaM Unit := do
-  let s := Format.pretty (format e)
-  dbg_trace "{s}"
-
-/-- What the expression looks like, but prettier  --/
-def logPrettyExpression (e : Expr) : MetaM Unit := do
-  dbg_trace "{←ppExpr e}"
-
-/-- What the expression looks like, but prettier  --/
-def logDelabExpression (e : Expr) : MetaM Unit := do
-  dbg_trace "{← Lean.PrettyPrinter.delab e}"
-
-/-- What type the expression compiles to  --/
-def logExpressionType (e : Expr) : MetaM Unit :=
-  do
-    let t ← inferType e
-    dbg_trace t
 
 /- Get (in a list) all subexpressions in an expression -/
 def getSubexpressionsIn (e : Expr) : List Expr :=
@@ -162,34 +80,6 @@ def getSubexpressionsIn (e : Expr) : List Expr :=
   let subexprs := subexprs.filter $ fun subexpr => !subexpr.hasLooseBVars -- remove the ones that will cause errors when parsing
   subexprs
 
-/-- Helper for incrementing idx when creating pretty names-/
-partial def mkPrettyNameHelper(hypNames : List Name) (base : Name) (i : Nat) : Name :=
-  let candidate := base.appendIndexAfter i
-  if (hypNames).contains candidate then
-    mkPrettyNameHelper hypNames base (i+1)
-  else
-    candidate
-
-/-- Names a function baseName_idx if that is available.  otherwise, names it baseName_idx+1 if available...and so on. -/
-def mkPrettyName (baseName : Name) (idx : Nat) : TacticM Name := do
-  return mkPrettyNameHelper (← getHypothesesNames) baseName idx
-
-/-- Gets all identifier names in an expression -/
-def getFreeIdentifiers (e : Expr) : List Name := e.getUsedConstants.toList
-
-/-- Create the expression d → b with name n-/
-def mkImplies (n : Name := `h) (d b : Expr) : MetaM Expr :=
-  return .forallE (← mkFreshUserName n) d b .default
-
-/-- Create a reasonable name for an expression -/
-def mkAbstractedName (n : Name) : Name :=
-    match n with
-    | (.str _ s) => s!"f_{s.takeWhile (fun c => c != '_')}" -- (fun c => c.isLower && c != '_')
-    | _ => `unknown
-
-#eval mkAbstractedName `prime_two
-#eval mkAbstractedName `instAddCommSemigroupInt
-
 /-- Returns true if "e" contains "subexpr".  Differs from "occurs" because this uses the coarser "isDefEq" rather than "==" -/
 def containsExpr(subexpr : Expr)  (e : Expr) : MetaM Bool := do
   let mctx ← getMCtx -- save metavar context before using isDefEq
@@ -198,18 +88,9 @@ def containsExpr(subexpr : Expr)  (e : Expr) : MetaM Bool := do
   setMCtx mctx -- revert metavar context after using isDefEq, so this function doesn't have side-effects on the expr e
   return firstExprContainingSubexpr.isSome
 
-/--This is the term that we are generalizing to an arbitrary term of that type -/
-structure GeneralizedTerm where
-  oldValue : Expr                 -- e.g. Hmul.hmul
-  name : Name         := `f       -- e.g. f
-  type : Expr                     -- e.g. ℕ → ℕ → ℕ
-  placeholder : Expr              -- e.g. .mvar #2383...to uniquely identify where it is
-deriving Repr
-
 /- Given a theorem name n, returns the theorem statement with each instance of "p" -/
 -- def replaceTheoremNameWithAbstractedTheoremStatement (n : Name) (p : Expr) : MetaM Expr :=
 -- do
-
 
 /- Replaces all instances of "p" in "e" with a metavariable.
 Roughly implemented like kabstract, with the following differences:
@@ -218,12 +99,13 @@ Roughly implemented like kabstract, with the following differences:
   kabstract doesn't perform unification to make sure different mvars that will ultimately unify are this same, this does
   kabstract doesn't look for instances of "p" in the types of constants, this does
 -/
-partial def abstract (e : Expr) (p : Expr) (occs : Occurrences := .all) : MetaM Expr := do
+partial def replacePatternWithMVars (e : Expr) (p : Expr) (occs : Occurrences := .all) : MetaM Expr := do
   -- let e ← instantiateMVars e
   let pType ← inferType p
   -- let pHeadIdx := p.toHeadIndex
   -- let pNumArgs := p.headNumArgs
   let rec visit (e : Expr) (offset : Nat) : StateRefT Nat MetaM Expr := do
+    logInfo m!"next visit"
     let visitChildren : Unit → StateRefT Nat MetaM Expr := fun _ => do
       match e with
       -- unify types of metavariables as soon as we get a chance in .app
@@ -252,8 +134,10 @@ partial def abstract (e : Expr) (p : Expr) (occs : Occurrences := .all) : MetaM 
       | e                => logInfo "matched here"; return e
 
     if e.hasLooseBVars then
+      -- return e
       logInfo m!"visiting children of {e} of type {e.ctorName}"
       visitChildren ()
+
     -- -- kabstract usually checks if the heads of the expressions are same before bothering to check definition equality
     -- -- this makes teh code more efficient, but it's not necessarily true that "heads not equal => not definitionally equal"
     -- else if e.toHeadIndex != pHeadIdx || e.headNumArgs != pNumArgs then
@@ -279,8 +163,8 @@ partial def abstract (e : Expr) (p : Expr) (occs : Occurrences := .all) : MetaM 
   visit e 0 |>.run' 1
 
 /-- Find the proof of the new auto-generalized theorem -/
-def autogeneralizeProof (thmProof : Expr) (f : GeneralizedTerm) : MetaM Expr := do
-  let abstractedProof ← abstract thmProof f.oldValue -- replace instances of f's old value with metavariables
+def autogeneralizeProof (thmProof : Expr) (fExpr : Expr) : MetaM Expr := do
+  let abstractedProof ← replacePatternWithMVars thmProof fExpr -- replace instances of f's old value with metavariables
 
   return abstractedProof
 
@@ -291,17 +175,16 @@ def autogeneralize (thmName : Name) (fExpr : Expr): TacticM Unit := do
   let (thmType, thmProof) := (← getHypothesisType thmName, ← getHypothesisProof thmName)
 
   -- Get details about the term we're going to generalize, to replace it with an arbitrary const of the same type
-  let f : GeneralizedTerm := {oldValue := fExpr, name := `f, type := ← inferType fExpr, placeholder := ← mkFreshExprMVar (some (← inferType fExpr))}
-  logInfo m!"The term to be generalized is {f.oldValue} with type {f.type}"
+  logInfo m!"The term to be generalized is {fExpr} with type {← inferType fExpr}"
 
   -- -- Get the generalized theorem (with those additional hypotheses)
-  let genThmProof ← autogeneralizeProof thmProof f; logInfo ("Generalized Proof: " ++ genThmProof)
+  let genThmProof ← autogeneralizeProof thmProof fExpr; logInfo ("Generalized Proof: " ++ genThmProof)
   let genThmType ← inferType genThmProof; logInfo ("Generalized Type: " ++ genThmType)
   let genThmType  ← instantiateMVars genThmType
 
   -- get the generalized type from user
-  let userThmType ← kabstract thmType f.oldValue (occs:= .pos [1])
-  let userThmType := userThmType.instantiate1 (← mkFreshExprMVar f.type)
+  let userThmType ← kabstract thmType fExpr (occs:= .pos [1])
+  let userThmType := userThmType.instantiate1 (← mkFreshExprMVar (← inferType fExpr))
 
   -- compare
   let unif ← isDefEq genThmType userThmType
@@ -318,9 +201,9 @@ def autogeneralize (thmName : Name) (fExpr : Expr): TacticM Unit := do
   let genThmType ← inferType genThmProof
   logInfo m!"Type with fvars instead of mvars: {genThmType}"
 
-  createHypothesis genThmType genThmProof (thmName++`Gen)
+  createLetHypothesis genThmType genThmProof (thmName++`Gen)
 
-  -- logInfo s!"Successfully generalized \n  {thmName} \nto \n  {thmName++`Gen} \nby abstracting \n  {← ppExpr fExpr}."
+  logInfo s!"Successfully generalized \n  {thmName} \nto \n  {thmName++`Gen} \nby abstracting \n  {← ppExpr fExpr}."
 
 /- Autogeneralize term "t" in hypothesis "h"-/
 elab "autogeneralize" h:ident f:term : tactic => do
