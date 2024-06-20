a729135c02475c5cdc71d59534082241e2c0123d
Mirek Olšák <mirek@olsak.net>
2024-06-20 16:08:43 +0100

    proof skeleton

--- MotivatedMoves/Mirek/Grasshopper.lean
+++ MotivatedMoves/Mirek/Grasshopper.lean
@@ -28,6 +28,8 @@ def Jumps.landings (jumps : Jumps) : MineField := jumps.bind (fun j => (jumpOver
 def Jumps.s (jumps : Jumps) : JumpSet := .ofList jumps
 def Jumps.sum (jumps : Jumps) : Int := jumps.s.sum
 
+macro "auto" : tactic => `(tactic | sorry)
+
 theorem order_jumps
 : ∀ jumps : JumpSet,
   ∃ jumpso : Jumps,
@@ -35,51 +37,51 @@ theorem order_jumps
 := by sorry
 
 theorem pop_max_jump
-: ∀ jumps : JumpSet,
-  jumps.sizeOf > 0 →
-  ∃ (j : Jump) (jumpsr : JumpSet),
+  (jumps : JumpSet)
+  (_ : jumps.sizeOf > 0 := by auto)
+: ∃ (j : Jump) (jumpsr : JumpSet),
   jumps = .cons j jumpsr ∧
   (∀ x ∈ jumps, x.length <= j.length)
 := by sorry
 
 theorem pop_first_jump
-: ∀ jumps : Jumps,
-  jumps.length > 0 →
-  ∃ (j : Jump) (jumpsr : Jumps),
+  (jumps : Jumps)
+  (_ : jumps.length > 0 := by auto)
+: ∃ (j : Jump) (jumpsr : Jumps),
   jumps = .cons j jumpsr
 := by sorry
 
 theorem split_mines
-: ∀ (mines : MineField) (i : ℤ),
-  i >= 0 →
-  i <= mines.length →
-  ∃ (mines0 mines1 : MineField),
+  (mines : MineField) (i : ℤ)
+  (_ : i >= 0 := by auto)
+  (_ : i <= mines.length := by auto)
+: ∃ (mines0 mines1 : MineField),
   mines = mines0 ++ mines1 ∧
   mines0.length = i
 := by sorry
 
 theorem split_first_mine
-: ∀ (mines : MineField),
-  mines.countMines > 0 →
-  ∃ (mines0 mines1 : MineField),
+  (mines : MineField)
+  (_ : mines.countMines > 0 := by auto)
+: ∃ (mines0 mines1 : MineField),
   mines = mines0 ++ [true] ++ mines1 ∧
   mines0.countMines = 0
 := by sorry
 
 theorem split_jump_landings
-: ∀ (jumps : Jumps),
-  i >= 0 →
-  i < jumps.sum →
-  ∃ (j : Jump) (jumps0 jumps1 : Jumps),
+  (jumps : Jumps) (i : Int)
+  (_ : i >= 0 := by auto)
+  (_ : i < jumps.sum := by auto)
+: ∃ (jumps0 : Jumps) (j : Jump) (jumps1 : Jumps),
   jumps = jumps0 ++ [j] ++ jumps1 ∧
   jumps0.sum <= i ∧
   jumps0.sum + j.length > i
 := by sorry
 
 theorem union_mines
-: ∀ (mines1 mines2 : MineField),
-  mines1.length = mines2.length →
-  ∃ (mines : MineField),
+  (mines1 mines2 : MineField)
+  (_ : mines1.length = mines2.length := by auto)
+: ∃ (mines : MineField),
   mines1.length = mines.length ∧
   mines2.length = mines.length ∧
   mines1.countMines <= mines.countMines ∧
@@ -89,16 +91,66 @@ theorem union_mines
   mines.countMines <= mines1.countMines + mines2.countMines
 := by sorry
 
-axiom main_jumps : Jumps
-axiom main_mines : MineField
-
-theorem grasshopper_ih
-: ∀ (jumps : JumpSet) (mines : MineField),
-  jumps.sizeOf < main_jumps.length →
-  jumps.Nodup →
-  jumps.sum = mines.length+1 →
-  jumps.sizeOf > mines.countMines →
-  ∃ (jumps_ih : Jumps),
-  jumps = jumps_ih.s ∧
-  (∀ (x : ℤ), ¬jumps_ih.landings[x] ∨ ¬mines[x])
-:= by sorry
+example
+  (main_jumps : JumpSet)
+  (main_mines : MineField)
+  (grasshopper_ih
+  : ∀ (jumps : JumpSet) (mines : MineField),
+    jumps.sizeOf < main_jumps.sizeOf →
+    jumps.Nodup →
+    jumps.sum = mines.length+1 →
+    jumps.sizeOf > mines.countMines →
+    ∃ (jumps_ih : Jumps),
+    jumps = jumps_ih.s ∧
+    (∀ (x : ℤ), ¬jumps_ih.landings[x] ∨ ¬mines[x])
+  ) :
+  main_jumps.Nodup →
+  main_jumps.sum = main_mines.length+1 →
+  main_jumps.sizeOf > main_mines.countMines →
+  ∃ (jumpso : Jumps),
+  main_jumps = jumpso.s ∧
+  (∀ (x : ℤ), ¬jumpso.landings[x] ∨ ¬main_mines[x])
+:= by
+  intros
+  by_cases main_mines.countMines = 0
+  -- mines are nonempty
+  · let ⟨jumpso, _⟩ := order_jumps main_jumps
+    use jumpso
+    auto
+  -- no mine on the first jump
+  · let ⟨J, jumps, _⟩ := pop_max_jump main_jumps
+    let ⟨mines0, mines1, _⟩ := split_mines main_mines J.length
+    let ⟨mines00, mines01, _⟩ := split_mines main_mines (J.length-1)
+    by_cases ¬ mines01.getIndexD 0
+    · by_cases mines0.countMines ≠ 0
+      -- mine before the first jump
+      · let ⟨jumpso, _⟩ := grasshopper_ih jumps mines1 (by auto) (by auto) (by auto) (by auto)
+        use [J] ++ jumpso
+        auto
+      -- no mine before the first jump
+      · let ⟨mines10, mines11, _⟩ := split_first_mine mines1
+        let ⟨jumpso, _⟩ := grasshopper_ih jumps (mines10 ++ [false] ++ mines11) (by auto) (by auto) (by auto) (by auto)
+        by_cases ¬ jumpso.landings.getIndexD mines10.length
+        -- no landing at the removed mine
+        · use [J] ++ jumpso
+          auto
+        -- landing at the removed mine
+        · let ⟨jumps0, J2, jumps1, _⟩ := split_jump_landings jumpso (mines10.length+1)
+          use jumps0 ++ [J2] ++ [J] ++ jumps1
+          auto
+    -- mine on the first jump
+    · by_cases mines00.length <= mines1.length
+      -- the first segment is smaller than the rest
+      · let ⟨mines10, mines11, _⟩ := split_mines mines1 mines00.length
+        let ⟨mines_un, _⟩ := union_mines mines00 mines10
+        let ⟨jumpso, _⟩ := grasshopper_ih jumps (mines_un ++ mines11) (by auto) (by auto) (by auto) (by auto)
+        let ⟨J2, jumpso, _⟩ := pop_first_jump jumpso
+        use [J2] ++ [J] ++ jumpso
+        auto
+      -- the first segment is bigger than the rest
+      · let ⟨mines00, _, _⟩ := split_mines mines00 mines1.length
+        let ⟨mines_un, _⟩ := union_mines mines00 mines1
+        let ⟨jumpso, _⟩ := grasshopper_ih jumps mines_un (by auto) (by auto) (by auto) (by auto)
+        let ⟨J2, jumpso, _⟩ := pop_first_jump jumpso
+        use [J2] ++ [J] ++ jumpso
+        auto
