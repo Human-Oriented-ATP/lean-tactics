5726c3c82e59cce272f2cc963ca1abe2bdeae0d0
Anshula Gandhi <anshula@users.noreply.github.com>
2024-07-16 12:58:08 +0100

    Update TutorialHelpers.lean

--- MotivatedMoves/AutoGeneralization/TutorialHelpers.lean
+++ MotivatedMoves/AutoGeneralization/TutorialHelpers.lean
@@ -2,6 +2,7 @@
 
 import Lean
 import Mathlib.Tactic
+import Qq open Qq
 open Lean Elab Tactic Meta Term Command
 
 /- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
@@ -59,6 +60,11 @@ def getHypothesisByName (h : Name) : TacticM LocalDecl := do
       return ldecl
   throwError "No hypothesis by that name."
 
+/-- Get the FVarID for a hypothesis (given its name) -/
+def getHypothesisFVarId (h : Name) : TacticM FVarId := do
+  let hyp ← getHypothesisByName h
+  return hyp.fvarId
+
 /-- Get the statement of a given hypothesis (given its name) -/
 def getHypothesisType (h : Name) : TacticM Expr := do
   let hyp ← getHypothesisByName h
@@ -102,19 +108,38 @@ Converting expressions to code (evaluation)
 /- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 Converting code to expressions (elaboration)
 - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -/
+
+/-- Turn Lean syntax into an expression --/
+def syntaxToExpr (e : TermElabM Syntax) : TermElabM Expr := do
+  let e ← elabTermAndSynthesize (← e) none
+  return e
+#eval syntaxToExpr `(2+3=5)
+
+/-- Print Lean terms as an expression --/
 elab "#term_to_expr" t:term : command => do
   let e ← liftTermElabM (Term.elabTerm t none)
-  logInfo m!"The expression corresponding to {t} is:\n\n{repr e}"
+  logInfo m!"{repr e}"
 #term_to_expr (2+3=5)
+
+/- - Turn Lean terms into an expression, using Qq -/
+#eval (q(2+3=5) : Expr)
 
 /- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 Checking types of expressions
 - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -/
 
-/--  Tactic to return T/F depending on the type of an expression -/
+/-- Tells you how an expression was constructed (for debugging purposes) -/
+def getExprConstructor (e: Expr): MetaM String := do
+  return e.ctorName
+#eval getExprConstructor (.bvar 0)      -- bvar
+#eval getExprConstructor (.const `n []) -- const
+
+/-- Tells you if an expression is a Nat (for debugging purposes) -/
 def isNat (e: Expr): MetaM Bool := do
   let type_expr ← inferType e
   return type_expr.isConstOf `Nat
+#eval isNat (q(3))    -- true
+#eval isNat (q(3.3))  -- false
 
 /- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 Printing expressions in varying degrees of detail
@@ -123,10 +148,28 @@ Printing expressions in varying degrees of detail
 /-- What the expression fully looks like --/
 def logExpression (e : Expr) : MetaM Unit := do
   dbg_trace "{repr e}"
+#eval logExpression (toExpr 2)
 
 /-- What the expression looks like, but with details hidden, so its prettier --/
 def logPrettyExpression (e : Expr) : MetaM Unit := do
   dbg_trace "{←ppExpr e}"
+#eval logPrettyExpression (toExpr 2)
+
+/- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+Getting theorems declared in the context
+- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -/
+/-- Getting theorem statement from context --/
+def getTheoremStatement (n : Name) : MetaM Expr := do
+  let some thm := (← getEnv).find? n | failure -- get the declaration with that name
+  return thm.type -- return the theorem statement
+#eval do {let e ← getTheoremStatement ``Nat.add_comm; logPrettyExpression e}
+
+/-- Getting theorem proof from context --/
+def getTheoremProof (n : Name) : MetaM Expr := do
+  let some thm := (← getEnv).find? n | failure -- get the declaration with that name
+  return thm.value! -- return the theorem statement
+#eval do {let e ← getTheoremProof ``Nat.add_comm; logPrettyExpression e}
+
 
 /- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 Getting subexpressions
@@ -144,46 +187,65 @@ def getSubexpressionsIn (e : Expr) : MetaM (List Expr) :=
     | Expr.proj _ _ b        => return [e] ++ (← getSubexpressionsInRec b acc)
     | _                      => return acc
   getSubexpressionsInRec e []
+#eval do {let e ← getTheoremStatement ``Nat.add_comm; for subexpr in (← getSubexpressionsIn e) do logPrettyExpression subexpr}
+
+/-- Returns true if "e" contains "subexpr".  Differs from "occurs" because this uses the coarser "isDefEq" rather than "==" -/
+def containsExpr(subexpr : Expr)  (e : Expr) : MetaM Bool := do
+  let mctx ← getMCtx -- save metavar context before using isDefEq
+  let e_subexprs ← getSubexpressionsIn e
+  let firstExprContainingSubexpr ← (e_subexprs.findM? fun e_subexpr => return ← isDefEq e_subexpr subexpr)
+  setMCtx mctx -- revert metavar context after using isDefEq, so this function doesn't have side-effects on the expr e
+  return firstExprContainingSubexpr.isSome
+
+/- Returns all subexpressions where "condition" holds -/
+def containsExprWhere (condition : Expr → Bool) (e : Expr)   : MetaM Bool := do
+  let e_subexprs ← getSubexpressionsIn e
+  let firstExprContainingSubexpr ← (e_subexprs.findM? fun e_subexpr => return condition e_subexpr)
+  return firstExprContainingSubexpr.isSome
 
 /- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-Getting theorems declared in the context
+Replacing subexpressions
 - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -/
 
-/-- Getting theorem statement from context --/
-def getTheoremStatement (n : Name) : MetaM Expr := do
-  let some thm := (← getEnv).find? n | failure -- get the declaration with that name
-  return thm.type -- return the theorem statement
+/- Replaces all subexpressions in "e" definitionally equal to "original" with a bound variable -/
+def replaceWithBVar (original : Expr) (e : Expr) : MetaM Expr :=
+  return ← kabstract e original
+#eval do logPrettyExpression $ ← replaceWithBVar (q(2)) (q(2+3=5)) -- replace "2" with a bvar in "2+3=5"
 
-/-- Getting theorem proof from context --/
-def getTheoremProof (n : Name) : MetaM Expr := do
-  let some thm := (← getEnv).find? n | failure -- get the declaration with that name
-  return thm.value! -- return the theorem statement
+/- Replaces all subexpressions in "e" definitionally equal to "original" with "replacement" -/
+def replaceWith (original : Expr) (replacement : Expr)  (e : Expr) : MetaM Expr := do
+  let abstractedExpr ← kabstract e original
+  return abstractedExpr.instantiate1 replacement
+#eval do logPrettyExpression $ ← replaceWith  (q(2)) (q(3)) (q(2+3=5))-- replace "2" with a "3" in "2+3=5"
 
 /- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 Getting metavariables in the context
 - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -/
 
-def printMVarsAssignmentsInContext : MetaM Unit := do
-  let m ← getMCtx
-  logInfo m!"MVar Context:
-    {(m.eAssignment.toList.map (fun d =>
-    m!"Mvar:
-      {d.fst.name}
-    Assignment:
-      {repr d.snd}"
 
-  ))}"
-
+/-- Returns all metavariable names and types in the context -/
 def printMVarContext : MetaM Unit := do
   let m ← getMCtx
   logInfo m!"MVar Context: {(m.decls.toList.map (fun d =>
-    m!"Name: {d.fst.name} Kind:{repr d.snd.kind} "
+    let mvarId := d.fst
+    let mvarDecl := d.snd
+    m!"Name: {mvarId.name} Type: {mvarDecl.type}"
   ))}"
 
+/-- Returns all ASSIGNED metavariable names and types -/
+def printMVarsAssignmentsInContext : MetaM Unit := do
+  let m ← getMCtx
+  logInfo m!"MVar Assignments: {(m.eAssignment.toList.map (fun d =>
+    m!"Mvar: {d.fst.name} Assignment: {d.snd}"
+  ))}"
+
+/-- Returns the expression that an mvar has been assigned to (if it has been assigned) -/
+def getAssignmentFor (m : MVarId) : MetaM (Option Expr) := do
+  let e ← getExprMVarAssignment? m
+  return e
 /- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 Getting all variables and instances in local context
 - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -/
-
 
 def printLocalContext : MetaM Unit := do
   let (lctx, linst) := (← getLCtx, ← getLocalInstances)
