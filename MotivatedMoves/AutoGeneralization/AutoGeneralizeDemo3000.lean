bcffc704321798231fd992cee0ea093d6ecfcd45
Anshula Gandhi <anshula@users.noreply.github.com>
2024-07-24 14:22:27 +0200

    renamed old to 2000

--- MotivatedMoves/AutoGeneralization/AutoGeneralizeDemo3000.lean
+++ MotivatedMoves/AutoGeneralization/AutoGeneralizeDemo3000.lean
@@ -1,7 +1,7 @@
-/- - - - - - - - - - - - - - - - - - - -
-Proof-based generalization
-- - - - - - - - - - - - - - - - - - - -/
-
+/- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+Proof-based generalization.
+Accounts for constants playing the same role in different places.
+- - - - - - - - - - - - - - - - - - - - - - -- - - - - - - - - - - - -/
 import Mathlib.FieldTheory.Finite.Basic
 import Mathlib.Analysis.InnerProductSpace.EuclideanDist
 import Mathlib.Data.Finset.Basic
@@ -12,20 +12,19 @@ open Autogeneralize library
 
 open Real
 open Lean Elab Tactic Meta Term Command
-
--- Uncomment below to hide proofs of "let" statements in the LeanInfoview
-set_option pp.showLetValues false
 -- set_option pp.explicit true
--- set_option pp.all true
 -- set_option profiler true
-set_option linter.unusedVariables false
 
+/- - - - - - - - - - - - - - - - - - - - - -
+Pretty-printing options for demo
+- - - - - - - - - - - - - - - - - - - - - -/
+set_option linter.unusedVariables false
+set_option pp.showLetValues false
 open  PrettyPrinter Delaborator in
 @[app_unexpander OfNat.ofNat]
 def unexpandOfNat : Unexpander
   | `($(_) $a) => `($a)
   | _ => throw ()
-
 
 
 /---------------------------------------------------------------------------
@@ -34,37 +33,15 @@ I.e. compatible proofs must use deduction rules, not computation rules
 ---------------------------------------------------------------------------/
 
 example := by
-  let two_times_three_is_even: Even (2*3) := by
-    simp only [Nat.reduceMul]; unfold Even; use 3
-  -- autogeneralize (3:ℕ) in two_times_three_is_even -- throws error
-  assumption
-
-example := by
-  let two_times_three_is_six : 2*3=6 := by simp only [Nat.reduceMul]
-  autogeneralize 3 in two_times_three_is_six
-  assumption
-
-/- --------------------------------------------------------------------------
-An example where "=3" is a required property, so the proof doesn't generalize.
--------------------------------------------------------------------------- -/
-
-theorem six_is_even : Even 6 := by
-  unfold Even
-  use 3
-
-example := by
-  let two_times_three_is_even : Even (2*3) := by
-    unfold Even; apply Exists.intro 3; rw [two_mul]
-
+  let two_times_three_is_even : Even (2*3) := by {unfold Even; apply Exists.intro 3; rw [two_mul]}
   autogeneralize 3 in two_times_three_is_even
-
-
   assumption
 
+theorem six_is_even : Even 6 := by {unfold Even; use 3}
 example := by
   let two_times_three_is_even : Even (2*3) := by
     simp only [Nat.reduceMul]; apply six_is_even
-  -- autogeneralize 3 in two_times_three_is_even -- throws error
+  -- autogeneralize 3 in two_times_three_is_even -- throws error b/c of computation rule
   assumption
 
 variable {α β : Type} [inst : Fintype α] [inst_1 : Fintype β] [inst_2 : DecidableEq α]
@@ -155,119 +132,16 @@ by
 
 example : True := by
   let _sum_irrat : Irrational ((sqrt (2:ℕ)) + 2) := by {apply Irrational.add_nat; apply Nat.prime_two.irrational_sqrt}
-
   autogeneralize (2:ℕ) in _sum_irrat
-
-
-
-
-
-
-
-
-
-
   specialize _sum_irrat.Gen 3 (Nat.prime_three) 6
   -- specialize _sum_irrat.Gen 3 (Nat.prime_three)
   simp
 
--- def Irrational' (y : ℝ) :  ∀ y: ℝ, y*y=(2:ℝ)
 
--- OPTION 1 def square_root (y : ℝ ) :
--- theorem sqrt2irr : ∀ y: ℝ, y*y=(2:ℝ) → Irrational y := sorry
--- theorem sqrt2plus2irr : ∀ y: ℝ, y*y=(2:ℝ) → Irrational (2+y) := sorry
-
--- OPTION 2 square root of absolute value of 2..
-
--- option 3 - -2 is still prime
-
--- option 4 - fix the proof for nats to not involve 1 + 2
-
-example  : True  := by
-
-  let _sqrt2Irrational : ¬ ∃ x : ℚ, x*x = (2:ℕ) := by {apply irrat_def'; intros h; obtain ⟨a, b, ⟨copr, h⟩⟩ := h; have a_div : 2 ∣ a := by {have c := (Prime.dvd_mul (Int.prime_two)).mp ((by apply (Iff.mpr dvd_iff_exists_eq_mul_right); use (b*b); rw [← mul_assoc]; rw [h]; rfl): 2 ∣ a*a); cases c; assumption; assumption}; have a_is_pk : ∃ k, a = 2 * k := by {apply (Iff.mp dvd_iff_exists_eq_mul_right) a_div}; obtain ⟨k, hk⟩ := a_is_pk; rw [hk] at h; replace h := Eq.symm h; rw [mul_assoc] at h; rw [mul_assoc] at h; apply Iff.mp (Int.mul_eq_mul_left_iff (Prime.ne_zero (Int.prime_two): (2:ℤ) ≠ 0)) at h; rw [mul_comm 2 k, ← mul_assoc] at h; have b_div : 2 ∣ b := by {have c := (Prime.dvd_mul (Int.prime_two)).mp ((by apply (Iff.mpr dvd_iff_exists_eq_mul_left); use (k*k)):2 ∣ b*b); cases c; assumption; assumption}; have p_dvd_gcd : 2 ∣ gcd a b := by {apply Iff.mpr (dvd_gcd_iff _ _ _) ⟨a_div, b_div⟩}; clear a_div b_div; rw [copr] at p_dvd_gcd; apply Prime.not_dvd_one (Int.prime_two) p_dvd_gcd}
-
-  autogeneralize_basic (2:ℕ) in _sqrt2Irrational
-  simp at _sqrt2Irrational.Gen
-  specialize _sqrt2Irrational.Gen 3 Int.prime_three
-
-  simp
-
-
-
-example  : True  := by
-
-  let _sqrt2Irrational : ¬ ∃ x : ℚ, x*x = (2:ℤ) := by {apply irrat_def'; intros h; obtain ⟨a, b, ⟨copr, h⟩⟩ := h; have a_div : 2 ∣ a := by {have c := (Prime.dvd_mul (Int.prime_two)).mp ((by apply (Iff.mpr dvd_iff_exists_eq_mul_right); use (b*b); rw [← mul_assoc]; rw [h]): 2 ∣ a*a); cases c; assumption; assumption}; have a_is_pk : ∃ k, a = 2 * k := by {apply (Iff.mp dvd_iff_exists_eq_mul_right) a_div}; obtain ⟨k, hk⟩ := a_is_pk; rw [hk] at h; replace h := Eq.symm h; rw [mul_assoc] at h; rw [mul_assoc] at h; apply Iff.mp (Int.mul_eq_mul_left_iff (Prime.ne_zero (Int.prime_two): (2:ℤ) ≠ 0)) at h; rw [mul_comm 2 k, ← mul_assoc] at h; have b_div : 2 ∣ b := by {have c := (Prime.dvd_mul (Int.prime_two)).mp ((by apply (Iff.mpr dvd_iff_exists_eq_mul_left); use (k*k)):2 ∣ b*b); cases c; assumption; assumption}; have p_dvd_gcd : 2 ∣ gcd a b := by {apply Iff.mpr (dvd_gcd_iff _ _ _) ⟨a_div, b_div⟩}; clear a_div b_div; rw [copr] at p_dvd_gcd; apply Prime.not_dvd_one (Int.prime_two) p_dvd_gcd}
-
-  autogeneralize_basic (2:ℤ ) in _sqrt2Irrational
-
-
-
-  have h := True
-
-
-
-
-
-
-
-
-  simp
 /- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 Example:
 sqrt(2) is irrational generalizes to sqrt(prime) is irrational
 - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -/
-
-
-example : True := by
-
-  let _sqrt2Irrational : Irrational (sqrt 2) := by apply Nat.prime_two.irrational_sqrt
-
-  autogeneralize_basic 2 in _sqrt2Irrational
-
-
-
-
-
-
-  simp
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-example : Irrational (sqrt 3) := by
-  let _sqrt2Irrational : Irrational (sqrt (2: ℕ)) := by apply Nat.prime_two.irrational_sqrt
-  autogeneralize_basic (2:ℕ) in _sqrt2Irrational -- adds _sqrt2Irrational.Gen to list of hypotheses
-
-  specialize _sqrt2Irrational.Gen 3 (Nat.prime_three)
-  assumption
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 /- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 Example of a naive, over-specialized generalization:
@@ -372,10 +246,9 @@ example :  ∀ (x y : EuclideanSpace ℝ (Fin 4)), dist x y = sqrt (Finset.sum F
 Analogizing a theorem about an operator that uses commutativity and associativity
 ---------------------------------------------------------------------------/
 example :  1 * 2 = 2 * 1 := by
-  -- let _multComm :  ∀ (n m : ℕ), n * m = m * n := by {intros n m; apply Nat.mul_comm}
   let _multComm :  ∀ (n m : ℕ), n * m = m * n :=  Nat.mul_comm
 
-  autogeneralize_basic Mul.mul in _multComm  -- (.*.) -- adds multPermute.Gen to list of hypotheses
+  autogeneralize_basic Mul.mul in _multComm  -- adds _multComm.Gen to list of hypotheses
 
   specialize _multComm.Gen Mul.mul Mul.mul Nat.mul_comm 1 2
   assumption
