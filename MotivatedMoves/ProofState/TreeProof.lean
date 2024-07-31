e7576c2f5abee9b2e89a060c865264d78426caf8
0art0 <18333981+0art0@users.noreply.github.com>
2024-07-31 16:33:09 +0100

    `PropBinder` instances

--- MotivatedMoves/ProofState/TreeProof.lean
+++ MotivatedMoves/ProofState/TreeProof.lean
@@ -1,9 +1,23 @@
 import Lean
+import Aesop
 import Qq
 
 open Lean Qq Elab Meta Term
 
 namespace MotivatedTree
+
+/-!
+
+# Structural sharing in the tactic state
+
+- The usual Lean tactic state is a list of meta-variables, each with its own local context.
+- This means that changes to the local context of one meta-variable do not affect the local context of the others.
+- This does not cause much of an issue while formalising known mathematical proofs, since one can always restructure the proof to change the local context before splitting into several goals.
+- However, this becomes problematic when using Lean to *discover* new proofs, since one may want to operate on a variable that occurs in the local context of multiple meta-variables.
+- The solution here is to represent the tactic state as a tree whose leaves are the open goals in the proofs and whose nodes contain the information of the variables in the local context of each goal.
+- Operating on a variable in the local context of one goal affects other goals too, since the change is made to the nodes of the tree.
+
+-/
 
 abbrev Tree := Prop
 
@@ -32,8 +46,63 @@ def TreeProof.map (step : ProofStep pol) (proof : TreeProof tree pol) : MetaM (T
     | .treeBackward newerTree newerTreeProof =>
       return .treeBackward newerTree q($newerTreeProof ∘ $proof)
 
-def TreeProof.compose (step₁ step₂ : ProofStep pol) : ProofStep pol := fun tree ↦ do
+def ProofStep.compose (step₁ step₂ : ProofStep pol) : ProofStep pol := fun tree ↦ do
   let treeProof ← step₁ tree
   treeProof.map step₂
+
+class PFunctor (f : Prop → Prop) where
+  map {p q : Prop} : (p → q) → f p → f q
+
+class PMonad (m : Prop → Prop) extends PFunctor m where
+  pure {p : Prop} : p → m p
+  bind {p q : Prop} : m p → (p → m q) → m q
+  map := fun f x ↦ bind x (Function.comp pure f)
+
+class PComonad (m : Prop → Prop) extends PFunctor m where
+  extract {p : Prop} : m p → p
+  extend {p q : Prop} : m p → (m p → q) → m q
+  map := fun f x ↦ extend x (f ∘ extract)
+
+class PropBinder (P : Q(Prop → Prop)) where
+  bind {tree : Q(Tree)} : TreeProof tree pol → TreeProof q($P $tree) pol
+
+class PropBinderContra (P : Q(Prop → Prop)) where
+  bind : TreeProof tree pol → TreeProof q($P $tree) !pol
+
+instance {m : Q(Prop → Prop)} (_inst : Q(PMonad $m)) : PropBinder q($m) where
+  bind
+  | .treeEnd treeProof => .treeEnd q(PMonad.pure $treeProof)
+  | .treeForward newTree proof => .treeForward q($m $newTree) q(PFunctor.map $proof)
+  | .treeBackward newTree proof => .treeBackward q($m $newTree) q(PFunctor.map $proof)
+
+instance {m : Q(Prop → Prop)} (_inst : Q(PComonad $m)) : PropBinder q($m) where
+  bind
+  | .treeEnd treeProof => .treeForward q($m True) q(PFunctor.map <| fun _ ↦ $treeProof)
+  | .treeForward newTree proof => .treeForward q($m $newTree) q(PFunctor.map $proof)
+  | .treeBackward newTree proof => .treeBackward q($m $newTree) q(PFunctor.map $proof)
+
+instance (p : Prop) : PMonad (p → ·) where
+  pure := fun x _ ↦ x
+  bind := fun f g x ↦ g (f x) x
+
+instance (p : Prop) : PComonad (p ∧ ·) where
+  extract := And.right
+  extend := fun ⟨x, y⟩ f => ⟨x, f ⟨x, y⟩⟩
+
+instance (p : Prop) : PComonad (· ∧ p) where
+  extract := And.left
+  extend := fun ⟨x, y⟩ f => ⟨f ⟨x, y⟩, y⟩
+
+instance (p : Prop) : PMonad (p ∨ ·) where
+  pure := Or.inr
+  bind
+  | Or.inl x => fun _ ↦ Or.inl x
+  | Or.inr y => fun f ↦ f y
+
+instance (p : Prop) : PMonad (· ∨ p) where
+  pure := Or.inl
+  bind
+  | Or.inl x => fun f ↦ f x
+  | Or.inr y => fun _ ↦ Or.inr y
 
 end MotivatedTree
