2962cf15062ea5c05aeddf267006261557021acd
0art0 <18333981+0art0@users.noreply.github.com>
2024-10-22 15:02:02 +0100

    Purge unrelated files

--- Tests/BreadthFirstSearch/Rewrite.lean
+++
@@ -1,9 +0,0 @@
-import MotivatedMoves.BreadthFirstSearch.Rewrite
-
-variables (a b : Nat)
-
-example (h : a = b) : a = b := by 
-  bfs_motivated_moves
-
-example (h1 : a = b) (h2 : b = c) : a = c := by 
-  bfs_motivated_moves
--- Tests/Moves/TreeContradiction.lean
+++
@@ -1,8 +0,0 @@
-import MotivatedMoves.Moves.TreeContradiction 
-
-example : (¬ p → q) → (¬ p → q) := by
-  make_tree
-  tree_contrapose [1,0] [1,1]
-  tree_apply [0,1] [1,0,1]
-  tree_push_neg [0]
-  tree_apply [0] [1]
