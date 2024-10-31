beaba3a5452362be38a08c7620069b194b33523e
Anshula Gandhi <anshula@users.noreply.github.com>
2024-10-31 13:11:36 +0000

    wrote proof product of odds is odd

--- MotivatedMoves/AutoGeneralization/AutoGeneralizeDemo4000.lean
+++ MotivatedMoves/AutoGeneralization/AutoGeneralizeDemo4000.lean
@@ -27,6 +27,51 @@ set_option pp.showLetValues false
 --   apply Subtype.fintype _
 -- instance [Fintype V'] : Fintype (G'ᶜ.neighborSet v') := by
 --   apply Subtype.fintype _
+
+/- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+PRODUCT OF ODDS IS ODD
+- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -/
+lemma product_of_odds (m n : Nat) : Odd m ∧ Odd n → Odd (m * n) := by
+  rintro ⟨⟨a,m_odd⟩ , ⟨b,n_odd⟩⟩
+  rw [Odd] at *
+  rw [m_odd]
+  rw [n_odd]
+  use (a*2*b + a + b)
+  rewrite [add_mul,mul_add, mul_add, mul_add, mul_add, mul_one, mul_one, one_mul]
+  -- simp only [add_mul, mul_add, mul_one, one_mul]
+  rewrite [mul_assoc, mul_assoc, add_assoc,add_assoc,add_assoc]
+  rfl
+  -- ring
+
+example : True := by
+  have product_of_odds := product_of_odds
+  trivial
+  -- autogeneralize 2 in product_of_odds
+#exit
+
+/- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+FOUR IS THE SUM OF TWO ODDS
+- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -/
+def four_is_sum_of_odds : ∃ m n : ℕ, Odd m ∧ Odd n ∧ m+n=4 := by
+  use 3
+  use 1
+  constructor
+  exact Nat.odd_iff.mpr rfl
+  constructor
+  -- simp only [odd_one]
+  exact Nat.odd_iff.mpr rfl
+  simp only [Nat.reduceAdd]
+
+example : True := by
+  have four_is_sum_of_odds := four_is_sum_of_odds
+  autogeneralize 4 in four_is_sum_of_odds -- exist 2 odds which sum to 3 + 1
+  autogeneralize 3 in four_is_sum_of_odds.Gen
+  simp at four_is_sum_of_odds.Gen.Gen
+  specialize four_is_sum_of_odds.Gen.Gen 7
+  dsimp at four_is_sum_of_odds.Gen.Gen
+  trivial
+
+
 /- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 SIX IS EVEN
 - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -/
@@ -109,93 +154,7 @@ example : True := by
   -- simp at impossible_graph.Gen.Gen
   trivial
 
-#exit
 
-
-/- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-TRYING OTHER MINIMAL EXAMPLES
-- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -/
-
-
-def th_minus_one : 4-1=3 := by
-  simp
-example : True := by
-  autogeneralize 4 in th_minus_one
-  trivial
-
-def test :=
-fun h =>
-  @Eq.casesOn Nat 2
-    (fun a t =>
-      @Eq Nat 0 a →
-        @HEq (@Eq Nat 2 0)
-            (@Eq.mp (@LE.le Nat instLENat 2 0) (@Eq Nat 2 0)
-              (@congrArg Prop Prop (@LE.le Nat instLENat 2 0) (@Eq Nat 2 0) (fun _a => _a)
-                (@propext (@LE.le Nat instLENat 2 0) (@Eq Nat 2 0) (@Nat.le_zero 2)))
-              (@Eq.mp (@LE.le Nat instLENat (Nat.succ 2) (Nat.succ 0)) (@LE.le Nat instLENat 2 0)
-                (@congrArg Prop Prop (@LE.le Nat instLENat (Nat.succ 2) (Nat.succ 0)) (@LE.le Nat instLENat 2 0)
-                  (fun _a => _a)
-                  (@propext (@LE.le Nat instLENat (Nat.succ 2) (Nat.succ 0)) (@LE.le Nat instLENat 2 0)
-                    (@Nat.succ_le_succ_iff 2 0)))
-                h))
-            (@Eq Nat 2 a) t →
-          False)
-    0
-    (@Eq.mp (@LE.le Nat instLENat 2 0) (@Eq Nat 2 0)
-      (@congrArg Prop Prop (@LE.le Nat instLENat 2 0) (@Eq Nat 2 0) (fun _a => _a)
-        (@propext (@LE.le Nat instLENat 2 0) (@Eq Nat 2 0) (@Nat.le_zero 2)))
-      (@Eq.mp (@LE.le Nat instLENat (Nat.succ 2) (Nat.succ 0)) (@LE.le Nat instLENat 2 0)
-        (@congrArg Prop Prop (@LE.le Nat instLENat (Nat.succ 2) (Nat.succ 0)) (@LE.le Nat instLENat 2 0) (fun _a => _a)
-          (@propext (@LE.le Nat instLENat (Nat.succ 2) (Nat.succ 0)) (@LE.le Nat instLENat 2 0)
-            (@Nat.succ_le_succ_iff 2 0)))
-        h))
-    (fun h_1 =>
-      @Nat.noConfusion
-        (@HEq (@Eq Nat 2 0)
-            (@Eq.mp (@LE.le Nat instLENat 2 0) (@Eq Nat 2 0)
-              (@congrArg Prop Prop (@LE.le Nat instLENat 2 0) (@Eq Nat 2 0) (fun _a => _a)
-                (@propext (@LE.le Nat instLENat 2 0) (@Eq Nat 2 0) (@Nat.le_zero 2)))
-              (@Eq.mp (@LE.le Nat instLENat (Nat.succ 2) (Nat.succ 0)) (@LE.le Nat instLENat 2 0)
-                (@congrArg Prop Prop (@LE.le Nat instLENat (Nat.succ 2) (Nat.succ 0)) (@LE.le Nat instLENat 2 0)
-                  (fun _a => _a)
-                  (@propext (@LE.le Nat instLENat (Nat.succ 2) (Nat.succ 0)) (@LE.le Nat instLENat 2 0)
-                    (@Nat.succ_le_succ_iff 2 0)))
-                h))
-            (@Eq Nat 2 2) (@Eq.refl Nat 2) →
-          False)
-        0 2 h_1)
-    (@Eq.refl Nat 0)
-    (@HEq.refl (@Eq Nat 2 0)
-      (@Eq.mp (@LE.le Nat instLENat 2 0) (@Eq Nat 2 0)
-        (@congrArg Prop Prop (@LE.le Nat instLENat 2 0) (@Eq Nat 2 0) (fun _a => _a)
-          (@propext (@LE.le Nat instLENat 2 0) (@Eq Nat 2 0) (@Nat.le_zero 2)))
-        (@Eq.mp (@LE.le Nat instLENat (Nat.succ 2) (Nat.succ 0)) (@LE.le Nat instLENat 2 0)
-          (@congrArg Prop Prop (@LE.le Nat instLENat (Nat.succ 2) (Nat.succ 0)) (@LE.le Nat instLENat 2 0)
-            (fun _a => _a)
-            (@propext (@LE.le Nat instLENat (Nat.succ 2) (Nat.succ 0)) (@LE.le Nat instLENat 2 0)
-              (@Nat.succ_le_succ_iff 2 0)))
-          h)))
-#print test
-
-open Qq in
-#eval show MetaM _ from do
-  let two_plus_one := q(Nat.succ 2)
-  let three_times_four := q((2 + 1)*4)
-  let four_times_four := q(4*4)
-  let three_is_even := q(Even 3)
-  let thmStatement := q(Nat.succ 2 ≤ Nat.succ 0 → False)
-  let three := q(3)
-  -- containsSubexpr  three two_plus_one -- true
-  -- containsSubexpr  three three_times_four -- true
-  -- containsSubexpr  three four_times_four -- false
-  -- containsSubexpr  three three_is_even -- true
-  -- containsSubexpr  three three -- true
-  containsSubexpr  three thmStatement -- true
-
-example : True := by
-  have test := test
-  autogeneralize (3:ℕ) in test
-  trivial
 
 /- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 GENERALIZING PROOFS OF SET SUMS - WITHOUT USING A LEMMA IN GENERALITY
@@ -224,6 +183,7 @@ example : ∀ (α : Type) [inst_2 : DecidableEq α] (A B : Finset α), A.card = 
   specialize union_of_finsets.Gen 3 4
   assumption
 
+#exit
 
 /- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 DIVISIBILITY RULE
@@ -245,91 +205,9 @@ theorem x_plus_x_is_even : ∀ x: ℕ, Even (x+x) := by
   aesop -- also closes teh goal
 
 
-
-
 /- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-FOUR IS THE SUM OF TWO ODDS
+DEPENDING ON X≠0 and not
 - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -/
-def four_is_sum_of_odds : ∃ m n : ℕ, Odd m ∧ Odd n ∧ m+n=4 := by
-  use 3
-  use 1
-  constructor
-  exact Nat.odd_iff.mpr rfl
-  constructor
-  -- simp only [odd_one]
-  exact Nat.odd_iff.mpr rfl
-  simp only [Nat.reduceAdd]
-
-example : True := by
-  autogeneralize 4 in four_is_sum_of_odds
-  trivial
-
-
-
-
-/- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-PRODUCT OF ODDS IS ODD
-- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -/
--- lemma product_of_odds (m n : Nat) : Odd m ∧ Odd n → Odd (m * n) := by
---   simp
---   by simp [not_or, Nat.even_mul, ← Nat.not_even_iff_odd]
-
-
-/- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-GENERALIZING PROOFS OF DEGREE SEQUENCES
-- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -/
-
-theorem hw_cardx : (Set.toFinset {w : Fin 4 | w ≠ v}).card = 3 := by
-    rw [@Set.toFinset_card]
-    simp only [ne_eq, Set.coe_setOf, Set.mem_setOf_eq, Fintype.card_subtype_compl, Fintype.card_fin, Fintype.card_ofSubsingleton, -Nat.reduceSub, -eq_self]
-    rfl
-    -- sorry
-    -- rw [eq_self]
-    -- exact Eq.refl (4-1)
-    -- exact Eq.refl (Nat.pred 4)
-
-example : True := by
-  autogeneralize (3:ℕ) in hw_cardx
-  -- autogeneralize (4:ℕ) in hw_cardx
-  trivial
-
-
-theorem hw_card : (Set.toFinset {w : Fin 4 | w ≠ v}).card = 3 := by
-    rw [@Set.toFinset_card]
-    simp only [ne_eq, Set.coe_setOf, Set.mem_setOf_eq, Fintype.card_subtype_compl, Fintype.card_fin, Fintype.card_ofSubsingleton, -Nat.reduceSub, -eq_self]
-    rfl
-    -- sorry
-    -- rw [eq_self]
-    -- exact Eq.refl (4-1)
-    -- exact Eq.refl (Nat.pred 4)
-
-example : True := by
-  -- autogeneralize (3:ℕ) in hw_card
-  -- autogeneralize (4:ℕ) in hw_card
-  trivial
-
-
-set_option simprocs false
-set_option trace.Meta.Tactic.simp.rewrite true
-theorem hw_card' : ∀ v : Fin 4, (Set.toFinset {w : Fin 4 | w ≠ v}).card = 4-1 := by
-    intro v
-    rw [@Set.toFinset_card]
-    set_option trace.Debug.Meta.Tactic.simp true in
-    simp only [ne_eq, Set.coe_setOf, Set.mem_setOf_eq, Fintype.card_subtype_compl, Fintype.card_fin, Fintype.card_ofSubsingleton, -Nat.reduceSub, -eq_self]
-    rfl
-    -- sorry
-    -- rw [eq_self]
-    -- exact Eq.refl (4-1)
-    -- exact Eq.refl (Nat.pred 4)
-
-example : ∀ v : Fin 5, (Set.toFinset {w : Fin 5 | w ≠ v}).card = 5-1 := by
-  autogeneralize (4:ℕ) in hw_card'
-  specialize hw_card'.Gen 5
-  assumption
-
-
-
-
 
 /--
 Fabian's example:
