74e8e8ea4ccf9e17ec95f8e9e7fbb74293919809
Anshula Gandhi <anshula@users.noreply.github.com>
2024-12-09 14:11:24 -0500

    trying products of nonevens

--- MotivatedMoves/AutoGeneralization/AutoGeneralizeDemo4000.lean
+++ MotivatedMoves/AutoGeneralization/AutoGeneralizeDemo4000.lean
@@ -25,15 +25,37 @@ set_option pp.showLetValues false
 /- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 PRODUCT OF ODDS IS ODD
 - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -/
-lemma product_of_odds' (a b : Nat) : Odd ((2*a+1)*(2*b+1)):= by
+lemma product_of_odds'  : ∀ a b : ℕ, Odd ((2*a+1)*(2*b+1)):= by
+  intros a b
   use (a*2*b + a + b)
   rewrite [add_mul,mul_add, mul_add, mul_add, mul_add, mul_one, mul_one, one_mul]
   rewrite [mul_assoc, mul_assoc, add_assoc,add_assoc,add_assoc]
   rfl
 
-example : True := by
+example : ∀ (a b : ℕ), ∃ k, (3 * a + 1) * (3 * b + 1) = 3 * k + 1 := by
   have product_of_odds' := product_of_odds'
   autogeneralize (2:ℕ) in product_of_odds'
+  specialize product_of_odds'.Gen 3
+  assumption
+
+
+
+
+
+
+example : ∀ (a b : ℕ), ∃ k, (3 * a + 1) * (3 * b + 1) = 3 * k + 1 := by
+  have product_of_odds' := product_of_odds'
+  -- autogeneralize (1:ℕ) in product_of_odds'
+  -- specialize product_of_odds'.Gen 3
+  -- assumption
+
+lemma product_of_nonevens (m n : Nat) : ¬Even m ∧ ¬Even n → ¬Even (m * n) := by
+  rintro ⟨m_not_even, n_not_even⟩
+  unfold Even at *
+  simp at *
+  intro k
+
+  sorry
 
 lemma product_of_odds (m n : Nat) : Odd m ∧ Odd n → Odd (m * n) := by
   rintro ⟨⟨a,m_odd⟩ , ⟨b,n_odd⟩⟩
@@ -42,17 +64,13 @@ lemma product_of_odds (m n : Nat) : Odd m ∧ Odd n → Odd (m * n) := by
   rw [n_odd]
   use (a*2*b + a + b)
   rewrite [add_mul,mul_add, mul_add, mul_add, mul_add, mul_one, mul_one, one_mul]
-  -- simp only [add_mul, mul_add, mul_one, one_mul]
   rewrite [mul_assoc, mul_assoc, add_assoc,add_assoc,add_assoc]
   rfl
-  -- ring
 
 example : True := by
   have product_of_odds := product_of_odds
   autogeneralize (2:ℕ) in product_of_odds
-  -- autogeneralize_basic (2:ℕ) in product_of_odds
-
-  -- trivial
+  -- need to modify tactic to see the "2" within the definition of "odd"
 #exit
 
 /- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
