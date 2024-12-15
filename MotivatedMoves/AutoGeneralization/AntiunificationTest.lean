a4a4db88c84f00b2cdbb682ef9c1459494c5feae
0art0 <18333981+0art0@users.noreply.github.com>
2024-12-15 23:08:55 +0000

    More tests for antiunification

--- MotivatedMoves/AutoGeneralization/AntiunificationTest.lean
+++ MotivatedMoves/AutoGeneralization/AntiunificationTest.lean
@@ -5,7 +5,16 @@ open Lean Elab Meta Command Term AntiUnify
 elab "#antiunify" e:term "&" e':term : command => runTermElabM fun _ ↦ do
   let e ← Term.elabTerm e none
   let e' ← Term.elabTerm e' none
-  let result ← leastCommonGeneralizer e e'
+  let (result, mismatches) ← antiUnify e e' |>.run []
   logInfo m!"{e}, {e'}\nResult: {result}"
+  logInfo "---\nMismatches:\n---"
+  for mismatch in mismatches do
+    logInfo m!"Left: {mismatch.left}\nRight: {mismatch.right}"
 
 #antiunify (((1 + 2) + 3) : Nat) & (((3 + 2) + 4) : Nat)
+
+#antiunify ∃ n : Nat, 1 + n = 2 & ∃ n : Nat, 2 + n = 2
+
+#antiunify (let n : Nat := 1 + 2; n + 5) & (let n : Nat := 1 + 3; n + 7)
+
+#antiunify ((1 + 2) + _ : Nat) & ((_ + 2) + 3 : Nat)
