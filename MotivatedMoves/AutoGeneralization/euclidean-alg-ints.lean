9e9219c31930afd66e2a84179ebf8608d27bf7d2
Anshula Gandhi <anshula@users.noreply.github.com>
2025-02-05 16:30:15 +0000

    formalized euclidean alg for integers!

--- MotivatedMoves/AutoGeneralization/euclidean-alg-ints.lean
+++ MotivatedMoves/AutoGeneralization/euclidean-alg-ints.lean
@@ -4,6 +4,14 @@ import Mathlib.Tactic
 import MotivatedMoves.AutoGeneralization.AutoGeneralizeTactic4000
 open Autogeneralize
 
+def Int.mod_decreasing {a b : ℤ}: a ≠ 0 → |b % a| < |a| := by
+  intro a_neq_0
+  rw [abs_of_nonneg (a := b % a)]
+  rw [← emod_abs]
+  refine emod_lt_of_pos b ?w₂.H
+  rwa [@abs_pos]
+  exact emod_nonneg b a_neq_0
+
 def Int.mod_dvd {a b k : ℤ}: k ∣ b % a → k ∣ a → k ∣ b := by
   intros h1 h2
   rw [← Int.ediv_add_emod b a]
@@ -24,5 +32,9 @@ def Int.hcf (a b : ℤ) : {g : ℤ // g ∣ a ∧ g ∣ b ∧ (∀ c, c ∣ a �
   else
     let ⟨val, ⟨ha, hb, hdiv⟩⟩ := Int.hcf (b % a) a
     ⟨val, ⟨hb, Int.mod_dvd ‹_› ‹_›, fun c hca hcr ↦ hdiv c (Int.dvd_mod ‹_› ‹_›) hca⟩⟩
-  termination_by a
-  decreasing_by sorry
+  termination_by (abs a).natAbs
+  decreasing_by (
+    have := Int.mod_decreasing h (b:=b)
+    refine natAbs_lt_natAbs_of_nonneg_of_lt ?w₁ this
+    exact abs_nonneg (b % a)
+  )
