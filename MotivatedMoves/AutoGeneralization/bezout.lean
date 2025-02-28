50dce71e9d387b487f5e47c9c6461b31b8e88fcf
Anshula Gandhi <anshula@users.noreply.github.com>
2025-02-28 10:36:35 +0000

    isolated bezout example -- unknown fvar error

--- MotivatedMoves/AutoGeneralization/bezout.lean
+++ MotivatedMoves/AutoGeneralization/bezout.lean
@@ -8,11 +8,27 @@ open Autogeneralize Classical
 def is_gcd (g a b : ℤ) : Prop := g ∣ a ∧ g ∣ b ∧ (∀ c, c ∣ a → c ∣ b → c ∣ g)
 notation g " is GCD[" a ", " b "]" => is_gcd g a b
 
--- def hcf (a b : ℤ) : ℤ := sorry
--- theorem hcf_iff {a b : ℤ} :
---   hcf a b = g ↔ g ∣ a ∧ g ∣ b ∧ (∀ c, c ∣ a → c ∣ b → c ∣ g) :=
--- by
---   sorry
+theorem bezout_mini (x y : ℤ) :
+    x ≠ 0 → y ≠ 0 →
+    let B := {z : ℕ   | ∃ h k : ℤ, z = (h * x + k * y).natAbs ∧ h * x + k * y ≠ 0};
+    B.Nonempty
+    :=
+
+by
+  intros _ y_neq_0
+  -- Show B is non-empty by constructing an element
+  use (0*x + 1*y).natAbs
+  use 0
+  use 1
+  constructor
+  rfl
+  simp only [zero_mul, one_mul, zero_add, ne_eq, y_neq_0, not_false_eq_true]
+
+example : True := by
+  autogeneralize ℤ in bezout_mini
+  trivial
+
+
 
 /-- Bézout's identity states that for any two integers a and b, there exist integers x and y such that their greatest common divisor g can be expressed as a linear combination ax + by = g -/
 theorem bezout_identity (x y : ℤ) :
@@ -236,3 +252,4 @@ by
 -- set_option trace.AntiUnify true
 example : True := by
   autogeneralize ℤ in bezout_identity
+  trivial
