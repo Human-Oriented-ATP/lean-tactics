521ca59208c41108acdaee18b31d43ddb268b6b2
0art0 <18333981+0art0@users.noreply.github.com>
2025-03-18 20:15:04 +0000

    New proof of Bezout's lemma

--- MotivatedMoves/AutoGeneralization/bezout.lean
+++ MotivatedMoves/AutoGeneralization/bezout.lean
@@ -10,243 +10,90 @@ set_option trace.ProofPrinting true
 def is_gcd (g a b : ℤ) : Prop := g ∣ a ∧ g ∣ b ∧ (∀ c, c ∣ a → c ∣ b → c ∣ g)
 notation g " is GCD[" a ", " b "]" => is_gcd g a b
 
-theorem bezout_mini' :
-    ∀ x y : ℤ,
-    y ≠ 0 →
-    ∃ h k : ℤ, h * x + k * y ≠ 0
-    :=
-
-by
-  intros x y y_neq_0
-  use 0
-  use 1
-  rw [zero_mul, one_mul, zero_add]
-  exact y_neq_0
-
-
-example : True := by
-  autogeneralize ℤ in bezout_mini'
-  trivial
-
-
-
+theorem Int.emod_natAbs_lt_of_nonzero (a : ℤ) {b : ℤ} (hb : b.natAbs ≠ 0) : (a % b).natAbs < b.natAbs := by
+  sorry
 
 /-- Bézout's identity states that for any two integers a and b, there exist integers x and y such that their greatest common divisor g can be expressed as a linear combination ax + by = g -/
-theorem bezout_identity (x y : ℤ) :
-  -- x ≠ 0 → y ≠ 0 → ∃ (h k : ℤ),  (hcf a b) = h * x + k * y  :=
-    x ≠ 0 → y ≠ 0 → ∃ (h k : ℤ), (h * x + k * y) is GCD[x, y] :=
-
-by
+theorem bezout_identity (x y : ℤ) : x ≠ 0 → y ≠ 0 → ∃ (h k : ℤ), (h * x + k * y) is GCD[x, y] := by
   intros x_neq_0 y_neq_0
 
   -- Consider the set A = {hx + ky | x,y ∈ ℤ}
   let A := {z : ℤ | ∃ h k : ℤ, z = h * x + k * y}
-  -- Consider the set B = {|z| | z ∈ A, z ≠ 0} of non-zero absolute values
-  let B := {z : ℕ   | ∃ h k : ℤ, z = (h * x + k * y).natAbs ∧ h * x + k * y ≠ 0}
-  -- let B := {z : ℕ | ∃ h k : ℤ, z = |h * x + k * y| ∧ h * x + k * y ≠ 0}
+  -- Consider the set B = {|z| | z ∈ A, |z| ≠ 0} of non-zero absolute values
+  let B := (Int.natAbs '' A) \ {0}
+
+  have A_add : ∀ a ∈ A, ∀ b ∈ A, a + b ∈ A := by
+    rintro a ⟨h, k, a_eq⟩ b ⟨h', k', b_eq⟩
+    use (h + h'), (k + k')
+    rw [a_eq, b_eq]
+    rw [add_mul, add_mul]
+    ac_rfl
+  have A_mul : ∀ a ∈ A, ∀ z : ℤ, z * a ∈ A := by
+    rintro a ⟨h, k, a_eq⟩ z
+    use z * h, z * k
+    rw [a_eq]
+    rw [mul_add, ← mul_assoc, ← mul_assoc]
 
   -- Show B is non-empty by constructing an element
-  have h_B_nonempty : B.Nonempty := by
+  have hB_nonempty : B.Nonempty := by
     use (0*x + 1*y).natAbs
-    use 0
-    use 1
-    constructor
-    rfl
-    simp only [zero_mul, one_mul, zero_add, ne_eq, y_neq_0, not_false_eq_true]
+    refine ⟨⟨_, ⟨?elem_in_A, rfl⟩⟩, ?elem_abs_nonZero⟩
+    · use 0, 1
+    · simpa only [zero_mul, one_mul, zero_add, Set.mem_singleton_iff, Int.natAbs_eq_zero]
 
   -- By well-ordering principle on subsets of ℕ, B has a minimal element
-  have h_B_min : ∃ d : ℕ, d ∈ B ∧ ∀ z ∈ B, d ≤ z := by
-
-    use Nat.find h_B_nonempty
-    constructor
-    · exact Nat.find_spec h_B_nonempty
-    · intro z hz
-      exact Nat.find_min' h_B_nonempty hz
-
-
   -- Call that minimal element "d"
-  let ⟨d, hd⟩ := h_B_min
-  clear h_B_nonempty
-  clear h_B_min
+  -- let Bmin := Nat.find hB_nonempty
+  let ⟨⟨d, (hdA : d ∈ A), (hdAbs_eq_Bmin : d.natAbs = _)⟩, (hBmin_neq_0 : _ ≠ 0)⟩ := Nat.find_spec hB_nonempty
+  have hdAbs_neq_0 : d.natAbs ≠ 0 := by rwa [← hdAbs_eq_Bmin] at hBmin_neq_0
+  have hd_min : ∀ z ∈ A, z.natAbs = 0 ∨ d.natAbs ≤ z.natAbs := by
+    intro z hz
+    by_cases hzAbs : z.natAbs = 0
+    · left
+      assumption
+    · right
+      have hBmin_min := Nat.find_min' hB_nonempty (m := z.natAbs)
+        ⟨Set.mem_image_of_mem Int.natAbs hz, hzAbs⟩
+      rwa [hdAbs_eq_Bmin]
 
-  -- Get h,k such that d = hx + ky
-  have ⟨h, k, d_eq, d_neq_zero⟩ := hd.1
-  use h
-  use k
-
-  -- Prove d > 0
-  have d_pos : 0 < d := by
-    rw [d_eq]
-    exact Int.natAbs_pos.mpr d_neq_zero
-  have d_neq_zero' : (d:ℤ) ≠ 0 := by exact Int.natCast_ne_zero_iff_pos.mpr d_pos
-  have d_pos' : (d:ℤ) > 0 := by exact Int.ofNat_pos.mpr d_pos
-
-
-
-  -- -- Prove c | x and c | y => c | d
-  -- have d_minimal : ∀ c, c ∣ x → c ∣ y → c ∣ d := by
-  --   intro c ⟨kc,c_div_x⟩ ⟨ky,c_div_y⟩
-  --   rw [d_eq, c_div_x, c_div_y]
-  --   simp only [Int.natCast_natAbs, dvd_abs]
-
-  --   refine (Int.dvd_add_left ?H).mpr ?_
-  --   rw [mul_comm, mul_assoc]
-  --   exact Int.dvd_mul_right c (ky * k)
-  --   rw [mul_comm, mul_assoc]
-  --   exact Int.dvd_mul_right c (kc * h)
-
-  -- Prove d | x
-  have d_dvd_x : (d:ℤ) ∣ x := by
-
+  have hd_div_A : ∀ a ∈ A, d ∣ a := by
+    intro a ha_A
     -- By division algorithm, x = qd + r for some q,r with 0 ≤ r < d
-    let q := x / d
-    let r := x % d
-    have x_eq : x = q*d+r  := Eq.symm (Int.ediv_add_emod' x ↑d)
-    -- have := Int.emod_nonneg x d_neq_zero'
-    have r_nonneg : 0 ≤ r := by apply Int.emod_nonneg x d_neq_zero'
-    have r_lt_d : r < d := by apply Int.emod_lt_of_pos x d_pos'
+    let q := a / d
+    let r := a % d
+    have a_eq_quotRem : a = q*d + r := Eq.symm (Int.ediv_add_emod' a ↑d)
+    have r_eq : r = (-q)*d + a := by
+      rw [← neg_add_eq_iff_eq_add, Int.neg_mul_eq_neg_mul] at a_eq_quotRem
+      symm; assumption
+    have rAbs_lt_dAbs : r.natAbs < d.natAbs := by
+      apply Int.emod_natAbs_lt_of_nonzero
+      assumption
+    have : r ∈ A := by
+      rw [r_eq]
+      apply A_add
+      · apply A_mul; assumption
+      · assumption
+    by_cases hr_eq_0 : r = 0
+    · rw [a_eq_quotRem, hr_eq_0, add_zero]
+      exact Int.dvd_mul_left q d
+    · rw [← Int.natAbs_eq_zero] at hr_eq_0
+      have hd_min_r := hd_min r
+      contrapose hd_min_r
+      push_neg
+      refine' ⟨_, _, _⟩ <;> assumption
 
-    -- If r ≠ 0, then r.natAbs ∈ B and r.natAbs < d, contradicting minimality
-    by_cases r_zero : r = 0
-    -- If r = 0, then d|x
-    rw [r_zero] at x_eq
-    use q; rw [x_eq]; simp only [add_zero, mul_comm]
+  have hxA : x ∈ A := by use 1, 0; simp only [one_mul, zero_mul, add_zero]
+  have d_dvd_x : d ∣ x := hd_div_A x hxA
 
-    -- If r ≠ 0, then r.natAbs ∈ B and r.natAbs < d, contradicting minimality of d
-    have r_in_A : (r:ℤ) ∈ A := by
-      have r_eq : r = x - q*d := by rw [x_eq]; ring_nf
+  have hyA : y ∈ A := by use 0, 1; simp only [zero_mul, one_mul, zero_add]
+  have d_dvd_y : d ∣ y := hd_div_A y hyA
 
-      -- Solve for r
-      -- x = q(hx + ky) + r
-      -- r = x - q(hx + ky)
-      -- r = x(1 - qh) - qky which is in A
-
-      by_cases d_sign : h*x + k*y > 0
-      · -- Case hx + ky > 0
-        use (1-q*h)
-        use (-q*k)
-
-        rw [r_eq, d_eq]
-
-        have d_abs_is_d : (h * x + k * y).natAbs = h * x + k * y := by
-          rw [Int.natCast_natAbs, abs_eq_self]
-          exact Int.le_of_lt d_sign
-        rw [d_abs_is_d]
-        ring_nf
-
-      · -- Case hx + ky ≤ 0
-        use (1+q*h)
-        use (q*k)
-
-        rw [r_eq, d_eq]
-
-        have d_abs_is_neg_d : (h * x + k * y).natAbs = -(h * x + k * y) := by
-          rw [Int.natCast_natAbs, abs_eq_neg_self]
-          exact Int.not_lt.mp d_sign
-        rw [d_abs_is_neg_d]
-        ring_nf
-
-    have r_abs_in_B : r.natAbs ∈ B := by
-      have ⟨hr,kr, r_eq_hk⟩  := r_in_A
-      use hr
-      use kr
-      constructor
-      rw [r_eq_hk]
-      rw [← r_eq_hk]; exact r_zero
-
-    -- This contradicts minimality of d
-    have d_le_r := hd.2 r.natAbs r_abs_in_B
-    clear r_abs_in_B r_in_A r_zero x_eq
-    by_contra ctra
-
-    have r_natabs_eq : r.natAbs = r := by
-      rw [Int.natCast_natAbs, abs_eq_self]; exact r_nonneg
-    rw [← r_natabs_eq] at r_lt_d
-    norm_cast at r_lt_d
-    have r_lt_r := lt_of_lt_of_le r_lt_d d_le_r
-    exact (lt_self_iff_false r.natAbs).mp r_lt_r
-
-  -- Prove d | y
-  have d_dvd_y : (d:ℤ) ∣ y := by
-
-    -- By division algorithm, y = qd + r for some q,r with 0 ≤ r < d
-    let q := y / d
-    let r := y % d
-    have y_eq : y = q*d+r  := Eq.symm (Int.ediv_add_emod' y ↑d)
-    -- have := Int.emod_nonneg x d_neq_zero'
-    have r_nonneg : 0 ≤ r := by apply Int.emod_nonneg y d_neq_zero'
-    have r_lt_d : r < d := by apply Int.emod_lt_of_pos y d_pos'
-
-    -- If r ≠ 0, then r.natAbs ∈ B and r.natAbs < d, contradicting minimality
-    by_cases r_zero : r = 0
-    -- If r = 0, then d|x
-    rw [r_zero] at y_eq
-    use q; rw [y_eq]; simp only [add_zero, mul_comm]
-
-    -- If r ≠ 0, then r.natAbs ∈ B and r.natAbs < d, contradicting minimality of d
-    have r_in_A : (r:ℤ) ∈ A := by
-      have r_eq : r = y - q*d := by rw [y_eq]; ring_nf
-
-      -- Solve for r
-      -- y = q(hx + ky) + r
-      -- r = y - q(hx + ky)
-      -- r = y - qhx - qky
-      -- r = y(1 - qk) - qhx which is in A
-
-      by_cases d_sign : h*x + k*y > 0
-      · -- Case hx + ky > 0
-        use (- q*h)
-        use (1-q*k)
-
-        rw [r_eq, d_eq]
-
-        have d_abs_is_d : (h * x + k * y).natAbs = h * x + k * y := by
-          rw [Int.natCast_natAbs, abs_eq_self]
-          exact Int.le_of_lt d_sign
-        rw [d_abs_is_d]
-        ring_nf
-
-      · -- Case hx + ky ≤ 0
-        use (q*h)
-        use (1+q*k)
-
-        rw [r_eq, d_eq]
-
-        have d_abs_is_neg_d : (h * x + k * y).natAbs = -(h * x + k * y) := by
-          rw [Int.natCast_natAbs, abs_eq_neg_self]
-          exact Int.not_lt.mp d_sign
-        rw [d_abs_is_neg_d]
-        ring_nf
-
-    have r_abs_in_B : r.natAbs ∈ B := by
-      have ⟨hr,kr, r_eq_hk⟩  := r_in_A
-      use hr
-      use kr
-      constructor
-      rw [r_eq_hk]
-      rw [← r_eq_hk]; exact r_zero
-
-    -- This contradicts minimality of d
-    have d_le_r := hd.2 r.natAbs r_abs_in_B
-    clear r_abs_in_B r_in_A r_zero y_eq
-    by_contra ctra
-
-    have r_natabs_eq : r.natAbs = r := by
-      rw [Int.natCast_natAbs, abs_eq_self]; exact r_nonneg
-    rw [← r_natabs_eq] at r_lt_d
-    norm_cast at r_lt_d
-    have r_lt_r := lt_of_lt_of_le r_lt_d d_le_r
-    exact (lt_self_iff_false r.natAbs).mp r_lt_r
-
-  rw [is_gcd]
-  refine' ⟨ _, _, _⟩
-  · rw [d_eq] at d_dvd_x
-    exact Int.natAbs_dvd.mp d_dvd_x
-  · rw [d_eq] at d_dvd_y
-    exact Int.natAbs_dvd.mp d_dvd_y
+  let ⟨h, k, d_eq⟩ := hdA
+  use h, k
+  refine' ⟨_, _, _⟩
+  · rwa [← d_eq]
+  · rwa [← d_eq]
   · intro c c_dvd_x c_dvd_y
-    -- specialize d_minimal c c_dvd_x c_dvd_y
-    -- rw [d_eq] at d_minimal
     exact Dvd.dvd.linear_comb c_dvd_x c_dvd_y h k
 
 -- set_option maxHeartbeats 200000
