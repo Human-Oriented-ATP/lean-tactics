f6decbfca3f987a5ce4b63cb15b0790b6048528e
0art0 <18333981+0art0@users.noreply.github.com>
2025-03-19 10:49:29 +0000

    Bezout part success

--- MotivatedMoves/AutoGeneralization/bezout.lean
+++ MotivatedMoves/AutoGeneralization/bezout.lean
@@ -38,9 +38,9 @@ theorem bezout_identity : ∀ (x y : ℤ), y ≠ 0 → ∃ (h k : ℤ), isGCD (h
   -- Consider the set B = {|z| | z ∈ A, |z| ≠ 0} of non-zero absolute values
   have A_add : ∀ a ∈ A, ∀ b ∈ A, a + b ∈ A := by
     rintro a ⟨h, k, a_eq⟩ b ⟨h', k', b_eq⟩
-    use (h + h'), (k + k')
+    use (discharger := skip) (h + h'), (k + k')
     rw [a_eq, b_eq]
-    rw [add_assoc, Int.add_left_comm (k * y) _ _, ← add_assoc, ← add_mul, ← add_mul]
+    rw [Int.add_assoc, Int.add_left_comm (k * y) _ _, ← Int.add_assoc, ← Int.add_mul, ← Int.add_mul]
   have A_mul : ∀ a ∈ A, ∀ z : ℤ, z * a ∈ A := by
     rintro a ⟨h, k, a_eq⟩ z
     use z * h, z * k
@@ -52,15 +52,19 @@ theorem bezout_identity : ∀ (x y : ℤ), y ≠ 0 → ∃ (h k : ℤ), isGCD (h
   have hB_nonempty : ∃ b : ℕ, b ∈ B := by
     use (0*x + 1*y).natAbs
     change (0*x + 1*y).natAbs ∈ Int.natAbs '' A \ {0}
-    rw [@Set.mem_diff_singleton]
+    rw [Set.mem_diff_singleton]
     constructor
     · apply Set.mem_image_of_mem Int.natAbs
       use (discharger := rfl) 0, 1
     · rwa [Int.zero_mul, Int.one_mul, Int.zero_add, @Ne.eq_def, Int.natAbs_eq_zero]
   -- By well-ordering principle on subsets of ℕ, B has a minimal element
   -- Call that minimal element "d"
-  -- let Bmin : ℕ := @Nat.find B _ hB_nonempty
-  obtain ⟨⟨d, (hdA : d ∈ A), (hdAbs_eq_Bmin : d.natAbs = _)⟩, (hBmin_neq_0 : _ ≠ 0)⟩ := Nat.find_spec hB_nonempty
+  let Bmin : ℕ := Nat.find hB_nonempty
+  have hBmin_spec : (∃ d ∈ A, d.natAbs = Bmin) ∧ Bmin ≠ 0 := Nat.find_spec hB_nonempty
+  rcases hBmin_spec.1 with ⟨d, hd⟩
+  have hdA := hd.1
+  have hdAbs_eq_Bmin:= hd.2
+  have hBmin_neq_0 := hBmin_spec.2
   have hdAbs_neq_0 : d.natAbs ≠ 0 := by rwa [← hdAbs_eq_Bmin] at hBmin_neq_0
   have hd_min : ∀ z ∈ A, z.natAbs = 0 ∨ d.natAbs ≤ z.natAbs := by
     intro z hz
@@ -77,44 +81,49 @@ theorem bezout_identity : ∀ (x y : ℤ), y ≠ 0 → ∃ (h k : ℤ), isGCD (h
     -- By division algorithm, x = qd + r for some q,r with 0 ≤ r < d
     let q := a / d
     let r := a % d
-    have a_eq_quotRem : a = q*d + r := Eq.symm (Int.ediv_add_emod' a ↑d)
+    have a_eq_quotRem : a = q*d + r := Eq.symm (Int.ediv_add_emod' a d)
     have r_eq : r = (-q)*d + a := by
       rw [← neg_add_eq_iff_eq_add, Int.neg_mul_eq_neg_mul] at a_eq_quotRem
       symm; assumption
     have rAbs_lt_dAbs : r.natAbs < d.natAbs := by
       apply Int.emod_natAbs_lt_of_nonzero
       assumption
-    have : r ∈ A := by
+    have hr_A : r ∈ A := by
       rw [r_eq]
       apply A_add
       · apply A_mul; assumption
       · assumption
-    by_cases hr_eq_0 : r = 0
-    · rw [a_eq_quotRem, hr_eq_0, add_zero]
-      exact dvd_mul_left d q
+    by_cases hr_eq_0 : r = (0 : ℤ)
+    · rw [a_eq_quotRem, hr_eq_0, Int.add_zero]
+      exact Int.dvd_mul_left q d
     · rw [← Int.natAbs_eq_zero] at hr_eq_0
-      have hd_min_r := hd_min r
-      sorry
-      -- contrapose hd_min_r
-      -- push_neg
-      -- refine' ⟨_, _, _⟩ <;> assumption
+      have hd_min_r := hd_min r hr_A
+      contrapose hd_min_r
+      push_neg
+      constructor <;> assumption
 
-  have hxA : x ∈ A := by use 1, 0; simp only [one_mul, zero_mul, add_zero]
+  have hxA : x ∈ A := by use 1, 0; simp only [Int.one_mul, Int.zero_mul, Int.add_zero]
   have d_dvd_x : d ∣ x := hd_div_A x hxA
 
-  have hyA : y ∈ A := by use 0, 1; simp only [zero_mul, one_mul, zero_add]
+  have hyA : y ∈ A := by use 0, 1; simp only [Int.zero_mul, Int.one_mul, Int.zero_add]
   have d_dvd_y : d ∣ y := hd_div_A y hyA
 
   obtain ⟨h, k, d_eq⟩ := hdA
-  use h, k
+  use (discharger := skip) h, k
+  rw [isGCD]
   refine' ⟨_, _, _⟩
   · rwa [← d_eq]
   · rwa [← d_eq]
-  · intro c c_dvd_x c_dvd_y
-    exact Dvd.dvd.linear_comb c_dvd_x c_dvd_y h k
+  · rintro c c_dvd_x c_dvd_y
+    rw [Int.dvd_def] at c_dvd_x c_dvd_y ⊢
+    rcases c_dvd_x with ⟨cx, hcx⟩
+    rcases c_dvd_y with ⟨cy, hcy⟩
+    use (h * cx + k * cy)
+    rw [Int.mul_add, Int.mul_left_comm c h _, Int.mul_left_comm c k _, ← hcx, ← hcy]
+    -- this gives errors
+    -- exact Dvd.dvd.linear_comb c_dvd_x c_dvd_y h k
 
 #print bezout_identity
-
 -- set_option maxHeartbeats 200000
 -- -- set_option trace.AntiUnify true
 example : True := by
