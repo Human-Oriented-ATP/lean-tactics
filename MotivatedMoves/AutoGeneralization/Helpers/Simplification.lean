2964d97329ff95560951dcdaedc76095789c0cce
0art0 <18333981+0art0@users.noreply.github.com>
2025-03-19 20:21:42 +0000

    Merge repeating typeclass hypotheses

--- MotivatedMoves/AutoGeneralization/Helpers/Simplification.lean
+++ MotivatedMoves/AutoGeneralization/Helpers/Simplification.lean
@@ -8,14 +8,14 @@ def performSimp (genThmType : Expr ) (genThmProof : Expr ): MetaM (Expr × Expr)
   let genThmProofSimp ← mkAppM `Eq.mpr #[← result.getProof, genThmProof]
   return (genThmTypeSimp, genThmProofSimp)
 
-
 /-- Unifies metavariables (which are hypotheses) when possible.  -/
 def removeRepeatingHypotheses (genThmProof : Expr) : MetaM Expr := do
   let hyps ← getMVars genThmProof
   for hyp₁ in hyps do
     for hyp₂ in hyps do
+      let hyp₁Type ← hyp₁.getType
       -- performs unificiation on propositions
-      if (← isProp <| ← hyp₁.getType') then do
+      if (← isProp hyp₁Type) || (← isClass? hyp₁Type).isSome then do
         -- `discard` ignores the result of its argument (but retains modifications to the state)
         -- `isDefEq` automatically rejects cases where the meta-variables have different types or have conflicting assignments
         discard <| isDefEq (.mvar hyp₁) (.mvar hyp₂)
